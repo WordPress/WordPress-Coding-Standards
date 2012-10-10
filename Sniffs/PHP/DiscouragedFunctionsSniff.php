9269b8992872b4ff6dea7181ea764bdb9b9b8993
Chris Olbekson <chris@c3mdigital.com>
2012-10-09 23:59:36 -0500

    Remove error_log() from discouraged functions and fixing syntax error.

--- Sniffs/PHP/DiscouragedFunctionsSniff.php
+++ Sniffs/PHP/DiscouragedFunctionsSniff.php
@@ -10,7 +10,7 @@
  */
 
 if (class_exists('Generic_Sniffs_PHP_ForbiddenFunctionsSniff', true) === false) {
-    throw new PHP_CodeSniffer_Exception('Class Generic_Sniffs_PHP_ForbiddenFunctionsSniff not found');
+	throw new PHP_CodeSniffer_Exception('Class Generic_Sniffs_PHP_ForbiddenFunctionsSniff not found');
 }
 
 /**
@@ -25,7 +25,7 @@ if (class_exists('Generic_Sniffs_PHP_ForbiddenFunctionsSniff', true) === false) 
 class WordPress_Sniffs_PHP_DiscouragedFunctionsSniff extends Generic_Sniffs_PHP_ForbiddenFunctionsSniff
 {
 
-    /**
+	/**
      * A list of forbidden functions with their alternatives.
      *
      * The value is NULL if no alternative exists. IE, the
@@ -33,42 +33,41 @@ class WordPress_Sniffs_PHP_DiscouragedFunctionsSniff extends Generic_Sniffs_PHP_
      *
      * @var array(string => string|null)
      */
-									 'error_log'     => null,
-									 'print_r'       => null,
-									 'ereg_replace'  => 'preg_replace',
-									 'ereg'          => null,
-									 'eregi_replace' => 'preg_replace',
-									 'split'         => null,
-									 'spliti'        => null,
-									 'var_dump'      => null,
-									 
-									 // WordPress
-									 'find_base_dir'            => 'WP_Filesystem::abspath',
-									 'get_base_dir'             => 'WP_Filesystem::abspath',
-									 'dropdown_categories'      => 'wp_link_category_checklist',
-									 'dropdown_link_categories' => 'wp_link_category_checklist',
-									 'get_link'                 => 'get_bookmark',
-									 'get_catname'              => 'get_cat_name',
-									 'register_globals'         => null,
-									 'wp_setcookie'             => 'wp_set_auth_cookie',
-									 'wp_get_cookie_login'      => null,
-									 'wp_login'                 => 'wp_signon',
-									 'get_the_attachment_link'  => 'wp_get_attachment_link',
-									 'get_attachment_icon_src'  => 'wp_get_attachment_image_src',
-									 'get_attachment_icon'      => 'wp_get_attachment_image',
-									 'get_attachment_innerHTML' => 'wp_get_attachment_image',
-									 'query_posts'              => 'WP_Query',
-									 'wp_reset_query'           => 'wp_reset_postdata',
+	protected $forbiddenFunctions = array(
 
-                                    );
+									'print_r'       => null,
+									'ereg_replace'  => 'preg_replace',
+									'ereg'          => null,
+									'eregi_replace' => 'preg_replace',
+									'split'         => null,
+									'spliti'        => null,
+									'var_dump'      => null,
 
-    /**
+									// WordPress
+									'find_base_dir'            => 'WP_Filesystem::abspath',
+									'get_base_dir'             => 'WP_Filesystem::abspath',
+									'dropdown_categories'      => 'wp_link_category_checklist',
+									'dropdown_link_categories' => 'wp_link_category_checklist',
+									'get_link'                 => 'get_bookmark',
+									'get_catname'              => 'get_cat_name',
+									'register_globals'         => null,
+									'wp_setcookie'             => 'wp_set_auth_cookie',
+									'wp_get_cookie_login'      => null,
+									'wp_login'                 => 'wp_signon',
+									'get_the_attachment_link'  => 'wp_get_attachment_link',
+									'get_attachment_icon_src'  => 'wp_get_attachment_image_src',
+									'get_attachment_icon'      => 'wp_get_attachment_image',
+									'get_attachment_innerHTML' => 'wp_get_attachment_image',
+									'query_posts'              => 'WP_Query',
+									'wp_reset_query'           => 'wp_reset_postdata',
+
+	);
+
+	/**
      * If true, an error will be thrown; otherwise a warning.
      *
      * @var bool
      */
-    public $error = true;
+	public $error = true;
 
 }//end class
-
-?>
