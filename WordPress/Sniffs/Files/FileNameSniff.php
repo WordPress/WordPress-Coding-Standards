7408fa4554e72b1070bd599a25896ed9c1be876c
JDGrimes <jdg@codesymphony.co>
2015-04-25 16:12:52 -0400

    Use correct method for skipping rest of file

--- WordPress/Sniffs/Files/FileNameSniff.php
+++ WordPress/Sniffs/Files/FileNameSniff.php
@@ -42,7 +42,7 @@ class WordPress_Sniffs_Files_FileNameSniff implements PHP_CodeSniffer_Sniff
      * @param int                  $stackPtr  The position of the current token in the
      *                                        stack passed in $tokens.
      *
-     * @return void
+     * @return int
      */
     public function process(PHP_CodeSniffer_File $phpcsFile, $stackPtr)
     {
@@ -55,7 +55,7 @@ class WordPress_Sniffs_Files_FileNameSniff implements PHP_CodeSniffer_Sniff
                 $phpcsFile->addError($error, $stackPtr, 'UnderscoresNotAllowed');
         }
 
-        $phpcsFile->removeTokenListener( $this, $this->register() );
+        return $phpcsFile->numTokens + 1;
 
     }//end process()
 
