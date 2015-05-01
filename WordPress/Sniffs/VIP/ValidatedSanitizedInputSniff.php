ba15db93e5526f21da979e6cbb4f7139c8a74e34
JDGrimes <jdg@codesymphony.co>
2015-05-01 18:09:46 -0400

    Move validation logic to a method
    This makes it available to all sniffs. We also move another method to
    the main sniff from the ValidatedSantizedInput sniff.
    
    In addition, I’ve refactored the sanitization portion of this sniff to
    use the `is_sanitized()` method that I recently introduced.

--- WordPress/Sniffs/VIP/ValidatedSanitizedInputSniff.php
+++ WordPress/Sniffs/VIP/ValidatedSanitizedInputSniff.php
@@ -64,102 +64,38 @@ class WordPress_Sniffs_VIP_ValidatedSanitizedInputSniff extends WordPress_Sniff
 		if ( ! in_array( $tokens[$stackPtr]['content'], $superglobals ) )
 			return;
 
-		$instance = $tokens[$stackPtr];
-		$varName = $instance['content'];
-
 		// If we're overriding a superglobal with an assignment, no need to test
 		if ( $this->is_assignment( $stackPtr ) ) {
 			return;
 		}
 
-		// Search for casting
-		$prev = $phpcsFile->findPrevious( array( T_WHITESPACE ), $stackPtr - 1, null, true, null, true );
-		$is_casted = in_array( $tokens[ $prev ]['code'], array( T_INT_CAST, T_DOUBLE_CAST, T_BOOL_CAST ) );
+		// This superglobal is being validated.
+		if ( $this->is_in_isset_or_empty( $stackPtr ) ) {
+			return;
+		}
 
-		if ( isset( $instance['nested_parenthesis'] ) ) {
-			$nested = $instance['nested_parenthesis'];
-			// Ignore if wrapped inside ISSET
-			end( $nested ); // Get closest parenthesis
-			if ( in_array( $tokens[ key( $nested ) - 1 ]['code'], array( T_ISSET, T_EMPTY, T_UNSET ) ) )
-				return;
-		} else {
+		// If this isn't in a function call we can perform the sanitization check easily.
+		if ( ! isset( $tokens[ $stackPtr ]['nested_parenthesis'] ) ) {
+
 			if ( $this->has_whitelist_comment( 'sanitization', $stackPtr ) ) {
 				return;
 			}
 
-			// Search for casting
-			$prev = $phpcsFile->findPrevious( array( T_WHITESPACE ), $stackPtr -1, null, true, null, true );
-			$is_casted = in_array( $tokens[ $prev ]['code'], array( T_INT_CAST, T_DOUBLE_CAST, T_BOOL_CAST ) );
-			if ( ! $is_casted ) {
+			// Search for casting.
+			if ( ! $this->is_safe_casted( $stackPtr ) ) {
 				$phpcsFile->addError( 'Detected usage of a non-sanitized input variable: %s', $stackPtr, 'InputNotSanitized', array( $tokens[$stackPtr]['content'] ) );
 				return;
 			}
 		}
 
+		$array_key = $this->get_array_access_key( $stackPtr );
 
-		$varKey = $this->getArrayIndexKey( $phpcsFile, $tokens, $stackPtr );
-
-		if ( empty( $varKey ) ) {
+		if ( empty( $array_key ) ) {
 			return;
 		}
 
-		// Check for validation first
-		$is_validated = false;
-
-		// Validation check in inner scope ?
-		if ( $this->check_validation_in_scope_only ) {
-			// Wrapped in a condition? check existence of isset with the variable as an argument
-			if ( ! empty( $tokens[$stackPtr]['conditions'] ) ) {
-				$conditions = $tokens[$stackPtr]['conditions'];
-				end( $conditions ); // Get closest condition
-				$conditionPtr = key( $conditions );
-				$condition = $tokens[$conditionPtr];
-
-				if ( isset( $condition['parenthesis_opener'] ) ) {
-					$scope_start = $condition['parenthesis_opener'];
-					$scope_end   = $condition['parenthesis_closer'];
-				}
-			}
-		} else {
-			// Get outer scope
-			$function = $phpcsFile->findPrevious( T_FUNCTION, $stackPtr );
-			if ( $function !== false && $stackPtr < $tokens[$function]['scope_closer'] ) {
-				$scope_start = $tokens[$function]['scope_opener'];
-				$scope_end = $stackPtr;
-			} else { // In the open air, check whole file
-				$scope_start = 0;
-				$scope_end = $stackPtr;
-			}
-		}
-
-		for ( $i = $scope_start + 1; $i < $scope_end; $i++ ) {
-			if ( ! in_array( $tokens[$i]['code'], array( T_ISSET, T_EMPTY, T_UNSET ) ) ) {
-				continue;
-			}
-			$issetPtr = $i;
-			if ( ! empty( $issetPtr ) ) {
-				$isset = $tokens[$issetPtr];
-				$issetOpener = $phpcsFile->findNext( T_OPEN_PARENTHESIS, $issetPtr );
-				$issetCloser = $tokens[$issetOpener]['parenthesis_closer'];
-
-				// Check that it is the same variable name
-				for ( $i = $issetOpener + 1; $i < $issetCloser; $i++ ) {
-					if ( $tokens[ $i ]['code'] != T_VARIABLE ) {
-						continue;
-					}
-
-					// Double check the $varKey inside the variable, ex: 'hello' in $_POST['hello']
-					$varKeyValidated = $this->getArrayIndexKey( $phpcsFile, $tokens, $i );
-
-					if ( $varKeyValidated == $varKey ) {
-						// everything matches, variable IS validated afterall ..
-						$is_validated = true;
-					}
-				}
-			}
-		}
-
-		if ( ! $is_validated ) {
+		// Check for validation first.
+		if ( ! $this->is_validated( $stackPtr, $array_key, $this->check_validation_in_scope_only ) ) {
 			$phpcsFile->addError( 'Detected usage of a non-validated input variable: %s', $stackPtr, 'InputNotValidated', array( $tokens[$stackPtr]['content'] ) );
 			// return; // Should we just return and not look for sanitizing functions ?
 		}
@@ -169,68 +105,11 @@ class WordPress_Sniffs_VIP_ValidatedSanitizedInputSniff extends WordPress_Sniff
 		}
 
 		// Now look for sanitizing functions
-		$is_sanitized = false;
-
-		if ( isset( $is_casted ) && $is_casted ) {
-			$is_sanitized = true;
-		} elseif ( isset( $nested ) ) {
-			$functionPtr = key( $nested ) - 1;
-			$function = $tokens[$functionPtr];
-			if ( T_STRING === $function['code'] ) {
-				$functionName = $function['content'];
-
-				if ( 'array_map' === $functionName ) {
-					$function_opener = $phpcsFile->findNext( T_OPEN_PARENTHESIS, $functionPtr + 1 );
-					$mapped_function = $phpcsFile->findNext( PHP_CodeSniffer_Tokens::$emptyTokens, $function_opener + 1, $function_opener['parenthesis_closer'], true );
-
-					if ( $mapped_function && T_CONSTANT_ENCAPSED_STRING === $tokens[ $mapped_function ]['code'] ) {
-						$functionName = trim( $tokens[ $mapped_function ]['content'], '\'' );
-					}
-				}
-
-				if (
-					in_array( $functionName, WordPress_Sniffs_XSS_EscapeOutputSniff::$autoEscapedFunctions )
-					||
-					in_array( $functionName, WordPress_Sniffs_XSS_EscapeOutputSniff::$sanitizingFunctions )
-					) {
-					$is_sanitized = true;
-				}
-			} elseif ( T_UNSET === $function['code'] ) {
-				$is_sanitized = true;
-			} elseif ( $is_casted ) {
-				$is_sanitized = true;
-			}
-		}
-
-		if ( ! $is_sanitized ) {
+		if ( ! $this->is_sanitized( $stackPtr ) ) {
 			$phpcsFile->addError( 'Detected usage of a non-sanitized input variable: %s', $stackPtr, 'InputNotSanitized', array( $tokens[$stackPtr]['content'] ) );
 		}
 
-
 		return;
 	}//end process()
-
-	/**
-	 * Get array index key of the variable requested
-	 * @param  [type] $phpcsFile [description]
-	 * @param  [type] $tokens    [description]
-	 * @param  [type] $stackPtr  [description]
-	 * @return [type]            [description]
-	 */
-	public function getArrayIndexKey( $phpcsFile, $tokens, $stackPtr ) {
-		// Find next bracket
-		$bracketOpener = $phpcsFile->findNext( array( T_OPEN_SQUARE_BRACKET ), $stackPtr, $stackPtr + 3 );
-
-		// If no brackets, exit with a warning, this is a non-typical usage of super globals
-		if ( empty ( $bracketOpener ) ) {
-			return false;
-		}
-
-		$bracketCloser = $tokens[$bracketOpener]['bracket_closer'];
-
-		$varKey = trim( $phpcsFile->getTokensAsString( $bracketOpener + 1, $bracketCloser - $bracketOpener - 1 ) ); // aka 'hello' in $_POST['hello']
-
-		return $varKey;
-	}
 
 }//end class
