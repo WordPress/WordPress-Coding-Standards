0fc4f46da52d5b9dd49b755ef1eefe992cbbda28
Weston Ruter <weston@xwp.co>
2016-03-14 15:23:37 -0700

    Add unit tests from @GaryJones; use WP coding standards

--- WordPress/Sniffs/WP/I18nSniff.php
+++ WordPress/Sniffs/WP/I18nSniff.php
@@ -8,86 +8,78 @@
  * @package   PHP_CodeSniffer
  * @author    Shady Sharaf <shady@x-team.com>
  */
-class WordPress_Sniffs_WP_I18nSniff implements PHP_CodeSniffer_Sniff
-{
+class WordPress_Sniffs_WP_I18nSniff implements PHP_CodeSniffer_Sniff {
 
-    public $i18n_functions = array(
-        'translate',
-        'translate_with_gettext_context',
-        '__',
-        'esc_attr__',
-        'esc_html__',
-        '_e',
-        'esc_attr_e',
-        'esc_html_e',
-        '_x',
-        '_ex',
-        'esc_attr_x',
-        'esc_html_x',
-        '_n',
-        '_nx',
-        '_n_noop',
-        '_nx_noop',
-        );
+	public $i18n_functions = array(
+		'translate',
+		'translate_with_gettext_context',
+		'__',
+		'esc_attr__',
+		'esc_html__',
+		'_e',
+		'esc_attr_e',
+		'esc_html_e',
+		'_x',
+		'_ex',
+		'esc_attr_x',
+		'esc_html_x',
+		'_n',
+		'_nx',
+		'_n_noop',
+		'_nx_noop',
+	);
 
-    /**
-     * Returns an array of tokens this test wants to listen for.
-     *
-     * @return array
-     */
-    public function register()
-    {
-        return array(
-                T_STRING,
-               );
+	/**
+	 * Returns an array of tokens this test wants to listen for.
+	 *
+	 * @return array
+	 */
+	public function register() {
+		return array(
+			T_STRING,
+		);
+	}
 
-    }//end register()
+	/**
+	 * Processes this test, when one of its tokens is encountered.
+	 *
+	 * @param PHP_CodeSniffer_File $phpcs_file The file being scanned.
+	 * @param int                  $stack_ptr  The position of the current token
+	 *                                        in the stack passed in $tokens.
+	 *
+	 * @return void
+	 */
+	public function process( PHP_CodeSniffer_File $phpcs_file, $stack_ptr ) {
+		$tokens = $phpcs_file->getTokens();
+		$token  = $tokens[ $stack_ptr ];
 
+		if ( ! in_array( $token['content'], $this->i18n_functions ) ) {
+			return;
+		}
 
-    /**
-     * Processes this test, when one of its tokens is encountered.
-     *
-     * @param PHP_CodeSniffer_File $phpcsFile The file being scanned.
-     * @param int                  $stackPtr  The position of the current token
-     *                                        in the stack passed in $tokens.
-     *
-     * @return void
-     */
-    public function process(PHP_CodeSniffer_File $phpcsFile, $stackPtr)
-    {
-        $tokens = $phpcsFile->getTokens();
-        $token  = $tokens[$stackPtr];
+		if ( $next_token = $phpcs_file->findNext( T_WHITESPACE, $stack_ptr + 1, null, true ) ) {
+			if ( T_OPEN_PARENTHESIS !== $tokens[ $next_token ]['code'] ) {
+				return;
+			}
+		}
 
-        if ( ! in_array( $token['content'], $this->i18n_functions ) ) {
-            return;
-        }
+		// Get arguments
+		for ( $i = $next_token + 1; $i < $tokens[ $next_token ]['parenthesis_closer'] - 1; $i += 1 ) {
+			if ( in_array( $tokens[ $i ]['code'], array( T_WHITESPACE, T_COMMA, T_CONSTANT_ENCAPSED_STRING ) ) ) {
+				continue;
+			}
 
-        if ( $nextToken = $phpcsFile->findNext( T_WHITESPACE, $stackPtr + 1, null, true ) ) {
-            if ( $tokens[$nextToken]['code'] != T_OPEN_PARENTHESIS ) {
-                return;
-            }
-        }
+			if ( T_DOUBLE_QUOTED_STRING === $tokens[ $i ]['code'] ) {
+				$string = $tokens[ $i ]['content'];
+				if ( preg_match( '#\$#', $string ) > 0 ) {
+					$phpcs_file->addError( 'Translatable strings should not contain variables, found ' . $tokens[ $i ]['content'], $i );
+					return;
+				}
+				continue;
+			}
 
-        // Get arguments
-        for ( $i = $nextToken + 1; $i < $tokens[$nextToken]['parenthesis_closer'] - 1; $i++ ) {
-            if ( in_array( $tokens[$i]['code'], array( T_WHITESPACE, T_COMMA,T_CONSTANT_ENCAPSED_STRING ) ) ) {
-                continue;
-            }
-
-            if ( $tokens[$i]['code'] === T_DOUBLE_QUOTED_STRING ) {
-                $string = $tokens[$i]['content'];
-                if ( preg_match( '#\$#', $string ) > 0 ) {
-                    $phpcsFile->addError( 'Translatable strings should not contain variables, found ' . $tokens[$i]['content'], $i );
-                    return;
-                }
-                continue;
-            }
-
-            $phpcsFile->addError( sprintf( 'Translatable string expected, but found "%s"', $tokens[$i]['content'] ), $i );
-            return;
-        }
-
-    }//end process()
-
-
-}//end class
+			$phpcs_file->addError( sprintf( 'Translatable string expected, but found "%s"', $tokens[ $i ]['content'] ), $i );
+			return;
+		}
+	}
+}
--- WordPress/Tests/WP/I18nUnitTest.php
+++ WordPress/Tests/WP/I18nUnitTest.php
@@ -8,42 +8,47 @@
  * @package   PHP_CodeSniffer
  * @author    Shady Sharaf <shady@x-team.com>
  */
-class WordPress_Tests_WP_I18nUnitTest extends AbstractSniffUnitTest
-{
+class WordPress_Tests_WP_I18nUnitTest extends AbstractSniffUnitTest {
 
+	/**
+	 * Returns the lines where errors should occur.
+	 *
+	 * The key of the array should represent the line number and the value
+	 * should represent the number of errors that should occur on that line.
+	 *
+	 * @return array(int => int)
+	 */
+	public function getErrorList() {
+		return array(
+			3 => 1,
+			7 => 1,
+			9 => 1,
+			11 => 1,
+			13 => 1,
+			15 => 1,
+			17 => 1,
+			19 => 1,
+			21 => 1,
+			23 => 1,
+			25 => 1,
+			31 => 1,
+			33 => 1,
+			35 => 1,
+			37 => 1,
+			39 => 1,
+			41 => 1,
+		 );
+	}
 
-    /**
-     * Returns the lines where errors should occur.
-     *
-     * The key of the array should represent the line number and the value
-     * should represent the number of errors that should occur on that line.
-     *
-     * @return array(int => int)
-     */
-    public function getErrorList()
-    {
-        return array(
-            3 => 1,
-            7 => 1,
-            9 => 1,
-            );
-
-    }//end getErrorList()
-
-
-    /**
-     * Returns the lines where warnings should occur.
-     *
-     * The key of the array should represent the line number and the value
-     * should represent the number of warnings that should occur on that line.
-     *
-     * @return array(int => int)
-     */
-    public function getWarningList()
-    {
-        return array();
-
-    }//end getWarningList()
-
-
-}//end class
+	/**
+	 * Returns the lines where warnings should occur.
+	 *
+	 * The key of the array should represent the line number and the value
+	 * should represent the number of warnings that should occur on that line.
+	 *
+	 * @return array(int => int)
+	 */
+	public function getWarningList() {
+		return array();
+	}
+}
