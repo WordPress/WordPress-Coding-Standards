4a01fbf76fd883997e52e4f276dc51c511b9e6da
jrfnl <github_nospam@adviesenzo.nl>
2016-07-15 00:37:03 +0200

    Add sniff to check for `add_..._page()` functions + unit tests.
    Closes #11.

---
+++ WordPress/Tests/Theme/NoAddAdminPagesUnitTest.php
@@ -0,0 +1,54 @@
+<?php
+/**
+ * Unit test class for the NoAddAdminPages sniff.
+ *
+ * A sniff unit test checks a .inc file for expected violations of a single
+ * coding standard. Expected errors and warnings are stored in this class.
+ *
+ * @category Theme
+ * @package  PHP_CodeSniffer
+ * @author   Juliette Reinders Folmer <wpplugins_nospam@adviesenzo.nl>
+ */
+class WordPress_Tests_Theme_NoAddAdminPagesUnitTest extends AbstractSniffUnitTest {
+
+	/**
+	 * Returns the lines where errors should occur.
+	 *
+	 * The key of the array should represent the line number and the value
+	 * should represent the number of errors that should occur on that line.
+	 *
+	 * @return array(int => int)
+	 */
+	public function getErrorList() {
+		return array(
+			4 => 1,
+			5 => 1,
+			6 => 1,
+			9 => 1,
+			12 => 1,
+			13 => 1,
+			14 => 1,
+			15 => 1,
+			16 => 1,
+			17 => 1,
+			18 => 1,
+			19 => 1,
+			20 => 1,
+		);
+
+	} // end getErrorList()
+
+	/**
+	 * Returns the lines where warnings should occur.
+	 *
+	 * The key of the array should represent the line number and the value
+	 * should represent the number of warnings that should occur on that line.
+	 *
+	 * @return array(int => int)
+	 */
+	public function getWarningList() {
+		return array();
+
+	} // end getWarningList()
+
+} // end class
