6f5bdf92da26b1783789aa52bc6c805fb1025ff0
Weston Ruter <weston@xwp.co>
2016-09-11 12:43:44 -0700

    Use implode() instead of join()

--- WordPress/Sniffs/WP/I18nSniff.php
+++ WordPress/Sniffs/WP/I18nSniff.php
@@ -257,7 +257,7 @@ class WordPress_Sniffs_WP_I18nSniff extends WordPress_Sniff {
 
 		if ( T_CONSTANT_ENCAPSED_STRING === $tokens[0]['code'] ) {
 			if ( 'domain' === $arg_name && ! empty( $this->text_domain ) && ! in_array( trim( $content, '\'""' ), $this->text_domain, true ) ) {
-				$phpcs_file->$method( 'Mismatch text domain. Expected \'%s\' but got %s.', $stack_ptr, 'TextDomainMismatch', array( join( "' or '", $this->text_domain ), $content ) );
+				$phpcs_file->$method( 'Mismatch text domain. Expected \'%s\' but got %s.', $stack_ptr, 'TextDomainMismatch', array( implode( "' or '", $this->text_domain ), $content ) );
 				return false;
 			}
 			return true;
@@ -272,7 +272,7 @@ class WordPress_Sniffs_WP_I18nSniff extends WordPress_Sniff {
 				return false;
 			}
 			if ( 'domain' === $arg_name && ! empty( $this->text_domain ) && ! in_array( trim( $content, '\'""' ), $this->text_domain, true ) ) {
-				$phpcs_file->$method( 'Mismatch text domain. Expected \'%s\' but got %s.', $stack_ptr, 'TextDomainMismatch', array( join( "' or '", $this->text_domain ), $content ) );
+				$phpcs_file->$method( 'Mismatch text domain. Expected \'%s\' but got %s.', $stack_ptr, 'TextDomainMismatch', array( implode( "' or '", $this->text_domain ), $content ) );
 				return false;
 			}
 			return true;
