afed022067d80caa74e2a554c3e20cbbb8143c09
jrfnl <github_nospam@adviesenzo.nl>
2016-10-05 04:20:38 +0200

    Revert code style changes and removal of commented out code to facilitate easier sync compare.
    Exclude file from WPCS codestyle check.

--- WordPress/Sniffs/Arrays/ArrayDeclarationSniff.php
+++ WordPress/Sniffs/Arrays/ArrayDeclarationSniff.php
@@ -58,17 +58,37 @@ class WordPress_Sniffs_Arrays_ArrayDeclarationSniff extends Squiz_Sniffs_Arrays_
 		if ( $tokens[ $lastContent ]['line'] === $tokens[ $arrayEnd ]['line'] ) {
 			$error = 'Closing parenthesis of array declaration must be on a new line';
 			$fix   = $phpcsFile->addFixableError( $error, $arrayEnd, 'CloseBraceNewLine' );
-			if ( true === $fix ) {
+			if ($fix === true) {
 				$phpcsFile->fixer->addNewlineBefore( $arrayEnd );
 			}
-		}
+			/*
+		} elseif ( $tokens[ $arrayEnd ]['column'] !== $keywordStart ) {
+			// Check the closing bracket is lined up under the "a" in array.
+			$expected = ( $keywordStart - 1 );
+			$found    = ( $tokens[ $arrayEnd ]['column'] - 1 );
+			$error    = 'Closing parenthesis not aligned correctly; expected %s space(s) but found %s';
+			$data     = array(
+				$expected,
+				$found,
+			);
+
+			$fix = $phpcsFile->addFixableError( $error, $arrayEnd, 'CloseBraceNotAligned', $data );
+			if ($fix === true) {
+				if ($found === 0) {
+					$phpcsFile->fixer->addContent( ( $arrayEnd - 1 ), str_repeat(' ', $expected ) );
+				} else {
+					$phpcsFile->fixer->replaceToken( ( $arrayEnd - 1 ), str_repeat(' ', $expected ) );
+				}
+			}
+			*/
+		} // end if
 
 		$keyUsed    = false;
 		$singleUsed = false;
 		$indices    = array();
 		$maxLength  = 0;
 
-		if ( T_ARRAY === $tokens[ $stackPtr ]['code'] ) {
+		if ($tokens[$stackPtr]['code'] === T_ARRAY) {
 			$lastToken = $tokens[ $stackPtr ]['parenthesis_opener'];
 		} else {
 			$lastToken = $stackPtr;
@@ -77,56 +97,58 @@ class WordPress_Sniffs_Arrays_ArrayDeclarationSniff extends Squiz_Sniffs_Arrays_
 		// Find all the double arrows that reside in this scope.
 		for ( $nextToken = ( $stackPtr + 1 ); $nextToken < $arrayEnd; $nextToken++ ) {
 			// Skip bracketed statements, like function calls.
-			if ( T_OPEN_PARENTHESIS === $tokens[ $nextToken ]['code']
-			    && ( ! isset( $tokens[ $nextToken ]['parenthesis_owner'] )
-			        || $tokens[ $nextToken ]['parenthesis_owner'] !== $stackPtr )
+			if ($tokens[$nextToken]['code'] === T_OPEN_PARENTHESIS
+				&& (isset($tokens[$nextToken]['parenthesis_owner']) === false
+					|| $tokens[ $nextToken ]['parenthesis_owner'] !== $stackPtr )
 			) {
 				$nextToken = $tokens[ $nextToken ]['parenthesis_closer'];
 				continue;
 			}
 
-			if ( in_array( $tokens[ $nextToken ]['code'], array( T_ARRAY, T_OPEN_SHORT_ARRAY, T_CLOSURE ), true ) ) {
+			if ( in_array( $tokens[ $nextToken ]['code'], array( T_ARRAY, T_OPEN_SHORT_ARRAY, T_CLOSURE ), true ) === true ) {
 				// Let subsequent calls of this test handle nested arrays.
-				if ( T_DOUBLE_ARROW !== $tokens[ $lastToken ]['code'] ) {
+				if ($tokens[$lastToken]['code'] !== T_DOUBLE_ARROW) {
 					$indices[] = array( 'value' => $nextToken );
 					$lastToken = $nextToken;
 				}
 
-				if ( T_ARRAY === $tokens[ $nextToken ]['code'] ) {
+				if ($tokens[$nextToken]['code'] === T_ARRAY) {
 					$nextToken = $tokens[ $tokens[ $nextToken ]['parenthesis_opener'] ]['parenthesis_closer'];
-				} elseif ( T_OPEN_SHORT_ARRAY === $tokens[ $nextToken ]['code'] ) {
+				} else if ($tokens[$nextToken]['code'] === T_OPEN_SHORT_ARRAY) {
 					$nextToken = $tokens[ $nextToken ]['bracket_closer'];
 				} else {
 					// T_CLOSURE.
-					$tokens[ $nextToken ]['scope_closer'];
+					$nextToken = $tokens[ $nextToken ]['scope_closer'];
 				}
 
 				$nextToken = $phpcsFile->findNext( T_WHITESPACE, ( $nextToken + 1 ), null, true );
-				if ( T_COMMA !== $tokens[ $nextToken ]['code'] ) {
+				if ($tokens[$nextToken]['code'] !== T_COMMA) {
 					$nextToken--;
 				} else {
 					$lastToken = $nextToken;
 				}
 
 				continue;
-			}
+			}//end if
 
-			if ( T_DOUBLE_ARROW !== $tokens[ $nextToken ]['code'] && T_COMMA !== $tokens[ $nextToken ]['code'] ) {
+			if ($tokens[$nextToken]['code'] !== T_DOUBLE_ARROW
+				&& $tokens[$nextToken]['code'] !== T_COMMA
+			) {
 				continue;
 			}
 
 			$currentEntry = array();
 
-			if ( T_COMMA === $tokens[ $nextToken ]['code'] ) {
+			if ($tokens[$nextToken]['code'] === T_COMMA) {
 				$stackPtrCount = 0;
-				if ( isset( $tokens[ $stackPtr ]['nested_parenthesis'] ) ) {
+				if (isset($tokens[$stackPtr]['nested_parenthesis']) === true) {
 					$stackPtrCount = count( $tokens[ $stackPtr ]['nested_parenthesis'] );
 				}
 
 				$commaCount = 0;
-				if ( isset( $tokens[ $nextToken ]['nested_parenthesis'] ) ) {
+				if (isset($tokens[$nextToken]['nested_parenthesis']) === true) {
 					$commaCount = count( $tokens[ $nextToken ]['nested_parenthesis'] );
-					if ( T_ARRAY === $tokens[ $stackPtr ]['code'] ) {
+					if ($tokens[$stackPtr]['code'] === T_ARRAY) {
 						// Remove parenthesis that are used to define the array.
 						$commaCount--;
 					}
@@ -139,8 +161,16 @@ class WordPress_Sniffs_Arrays_ArrayDeclarationSniff extends Squiz_Sniffs_Arrays_
 					continue;
 				}
 
-				if ( false === $keyUsed ) {
-					if ( T_WHITESPACE === $tokens[ ( $nextToken - 1 ) ]['code'] ) {
+				/*
+				if ($keyUsed === true && $tokens[$lastToken]['code'] === T_COMMA) {
+					$error = 'No key specified for array entry; first entry specifies key';
+					$phpcsFile->addError( $error, $nextToken, 'NoKeySpecified' );
+					return;
+				}
+				*/
+
+				if ($keyUsed === false) {
+					if ($tokens[($nextToken - 1)]['code'] === T_WHITESPACE) {
 						$content = $tokens[ ( $nextToken - 2 ) ]['content'];
 						if ( $tokens[ ( $nextToken - 1 ) ]['content'] === $phpcsFile->eolChar ) {
 							$spaceLength = 'newline';
@@ -155,7 +185,7 @@ class WordPress_Sniffs_Arrays_ArrayDeclarationSniff extends Squiz_Sniffs_Arrays_
 						);
 
 						$fix = $phpcsFile->addFixableError( $error, $nextToken, 'SpaceBeforeComma', $data );
-						if ( true === $fix ) {
+						if ($fix === true) {
 							$phpcsFile->fixer->replaceToken( ( $nextToken - 1 ), '' );
 						}
 					}
@@ -169,14 +199,21 @@ class WordPress_Sniffs_Arrays_ArrayDeclarationSniff extends Squiz_Sniffs_Arrays_
 
 					$indices[]  = array( 'value' => $valueContent );
 					$singleUsed = true;
-				} // End if().
+				}//end if
 
 				$lastToken = $nextToken;
 				continue;
 
-			} // End if().
+			}//end if
 
-			if ( T_DOUBLE_ARROW === $tokens[ $nextToken ]['code'] ) {
+			if ($tokens[$nextToken]['code'] === T_DOUBLE_ARROW) {
+				/*
+				if ($singleUsed === true) {
+					$error = 'Key specified for array entry; first entry has no key';
+					$phpcsFile->addError( $error, $nextToken, 'KeySpecified' );
+					return;
+				}
+				*/
 
 				$currentEntry['arrow'] = $nextToken;
 				$keyUsed               = true;
@@ -209,23 +246,55 @@ class WordPress_Sniffs_Arrays_ArrayDeclarationSniff extends Squiz_Sniffs_Arrays_
 				$currentEntry['value'] = $nextContent;
 				$indices[]             = $currentEntry;
 				$lastToken             = $nextToken;
-			} // End if().
-		} // End for().
+			}//end if
+		}//end for
 
 		// Check for mutli-line arrays that should be single-line.
 		$singleValue = false;
 
-		if ( empty( $indices ) ) {
+		if (empty($indices) === true) {
 			$singleValue = true;
-		} elseif ( 1 === count( $indices ) && T_COMMA === $tokens[ $lastToken ]['code'] ) {
+		} else if (count($indices) === 1 && $tokens[$lastToken]['code'] === T_COMMA) {
 			// There may be another array value without a comma.
 			$exclude     = PHP_CodeSniffer_Tokens::$emptyTokens;
 			$exclude[]   = T_COMMA;
 			$nextContent = $phpcsFile->findNext( $exclude, ( $indices[0]['value'] + 1 ), $arrayEnd, true );
-			if ( false === $nextContent ) {
+			if ($nextContent === false) {
 				$singleValue = true;
 			}
 		}
+
+		/*
+		if ($singleValue === true) {
+			// Array cannot be empty, so this is a multi-line array with
+			// a single value. It should be defined on single line.
+			$error = 'Multi-line array contains a single value; use single-line array instead';
+			$fix   = $phpcsFile->addFixableError( $error, $stackPtr, 'MultiLineNotAllowed' );
+
+			if ($fix === true) {
+				$phpcsFile->fixer->beginChangeset();
+				for ( $i = ( $arrayStart + 1 ); $i < $arrayEnd; $i++ ) {
+					if ($tokens[$i]['code'] !== T_WHITESPACE) {
+						break;
+					}
+
+					$phpcsFile->fixer->replaceToken( $i, '' );
+				}
+
+				for ( $i = ( $arrayEnd - 1 ); $i > $arrayStart; $i-- ) {
+					if ($tokens[$i]['code'] !== T_WHITESPACE) {
+						break;
+					}
+
+					$phpcsFile->fixer->replaceToken( $i, '' );
+				}
+
+				$phpcsFile->fixer->endChangeset();
+			}
+
+			return;
+		} // end if
+		*/
 
 		/*
 			This section checks for arrays that don't specify keys.
@@ -238,7 +307,7 @@ class WordPress_Sniffs_Arrays_ArrayDeclarationSniff extends Squiz_Sniffs_Arrays_
 			   );
 		*/
 
-		if ( false === $keyUsed && ! empty( $indices ) ) {
+		if ($keyUsed === false && empty($indices) === false) {
 			$count     = count( $indices );
 			$lastIndex = $indices[ ( $count - 1 ) ]['value'];
 
@@ -249,11 +318,11 @@ class WordPress_Sniffs_Arrays_ArrayDeclarationSniff extends Squiz_Sniffs_Arrays_
 				true
 			);
 
-			if ( T_COMMA !== $tokens[ $trailingContent ]['code'] ) {
+			if ($tokens[$trailingContent]['code'] !== T_COMMA) {
 				$phpcsFile->recordMetric( $stackPtr, 'Array end comma', 'no' );
 				$error = 'Comma required after last value in array declaration';
 				$fix   = $phpcsFile->addFixableError( $error, $trailingContent, 'NoCommaAfterLast' );
-				if ( true === $fix ) {
+				if ($fix === true) {
 					$phpcsFile->fixer->addContent( $trailingContent, ',' );
 				}
 			} else {
@@ -262,28 +331,51 @@ class WordPress_Sniffs_Arrays_ArrayDeclarationSniff extends Squiz_Sniffs_Arrays_
 
 			$lastValueLine = false;
 			foreach ( $indices as $value ) {
-				if ( empty( $value['value'] ) ) {
+				if (empty($value['value']) === true) {
 					// Array was malformed and we couldn't figure out
 					// the array value correctly, so we have to ignore it.
 					// Other parts of this sniff will correct the error.
 					continue;
 				}
 
-				if ( false !== $lastValueLine && $tokens[ $value['value'] ]['line'] === $lastValueLine ) {
+				if ($lastValueLine !== false && $tokens[$value['value']]['line'] === $lastValueLine) {
 					$error = 'Each value in a multi-line array must be on a new line';
 					$fix   = $phpcsFile->addFixableError( $error, $value['value'], 'ValueNoNewline' );
-					if ( true === $fix ) {
-						if ( T_WHITESPACE === $tokens[ ( $value['value'] - 1 ) ]['code'] ) {
+					if ($fix === true) {
+						if ($tokens[($value['value'] - 1)]['code'] === T_WHITESPACE) {
 							$phpcsFile->fixer->replaceToken( ( $value['value'] - 1 ), '' );
 						}
 
 						$phpcsFile->fixer->addNewlineBefore( $value['value'] );
 					}
-				}
+					/*
+				} else if ($tokens[($value['value'] - 1)]['code'] === T_WHITESPACE) {
+					$expected = $keywordStart;
+
+					$first = $phpcsFile->findFirstOnLine( T_WHITESPACE, $value['value'], true );
+					$found = ($tokens[ $first ]['column'] - 1);
+					if ( $found !== $expected ) {
+						$error = 'Array value not aligned correctly; expected %s spaces but found %s';
+						$data  = array(
+							$expected,
+							$found,
+						);
+
+						$fix = $phpcsFile->addFixableError( $error, $value['value'], 'ValueNotAligned', $data );
+						if ($fix === true) {
+							if ($found === 0) {
+								$phpcsFile->fixer->addContent( ( $value['value'] - 1 ), str_repeat(' ', $expected ) );
+							} else {
+								$phpcsFile->fixer->replaceToken( ( $value['value'] - 1 ), str_repeat(' ', $expected ) );
+							}
+						}
+					}
+					*/
+				} // end if
 
 				$lastValueLine = $tokens[ $value['value'] ]['line'];
-			}
-		} // End if().
+			}//end foreach
+		}//end if
 
 		/*
 			Below the actual indentation of the array is checked.
@@ -316,14 +408,16 @@ class WordPress_Sniffs_Arrays_ArrayDeclarationSniff extends Squiz_Sniffs_Arrays_
 
 		$indicesStart  = ( $keywordStart + 1 );
 		$arrowStart    = ( $indicesStart + $maxLength + 1 );
+		//$valueStart    = ($arrowStart + 3);
 		$indexLine     = $tokens[ $stackPtr ]['line'];
+		//$lastIndexLine = null;
 		foreach ( $indices as $index ) {
-			if ( ! isset( $index['index'] ) ) {
+			if (isset($index['index']) === false) {
 				// Array value only.
 				if ( $tokens[ $index['value'] ]['line'] === $tokens[ $stackPtr ]['line'] && $numValues > 1 ) {
 					$error = 'The first value in a multi-value array must be on a new line';
 					$fix   = $phpcsFile->addFixableError( $error, $stackPtr, 'FirstValueNoNewline' );
-					if ( true === $fix ) {
+					if ($fix === true) {
 						$phpcsFile->fixer->addNewlineBefore( $index['value'] );
 					}
 				}
@@ -337,7 +431,7 @@ class WordPress_Sniffs_Arrays_ArrayDeclarationSniff extends Squiz_Sniffs_Arrays_
 			if ( $indexLine === $tokens[ $stackPtr ]['line'] ) {
 				$error = 'The first index in a multi-value array must be on a new line';
 				$fix   = $phpcsFile->addFixableError( $error, $index['index'], 'FirstIndexNoNewline' );
-				if ( true === $fix ) {
+				if ($fix === true) {
 					$phpcsFile->fixer->addNewlineBefore( $index['index'] );
 				}
 
@@ -347,8 +441,8 @@ class WordPress_Sniffs_Arrays_ArrayDeclarationSniff extends Squiz_Sniffs_Arrays_
 			if ( $indexLine === $lastIndexLine ) {
 				$error = 'Each index in a multi-line array must be on a new line';
 				$fix   = $phpcsFile->addFixableError( $error, $index['index'], 'IndexNoNewline' );
-				if ( true === $fix ) {
-					if ( T_WHITESPACE === $tokens[ ( $index['index'] - 1 ) ]['code'] ) {
+				if ($fix === true) {
+					if ($tokens[($index['index'] - 1)]['code'] === T_WHITESPACE) {
 						$phpcsFile->fixer->replaceToken( ( $index['index'] - 1 ), '' );
 					}
 
@@ -358,46 +452,122 @@ class WordPress_Sniffs_Arrays_ArrayDeclarationSniff extends Squiz_Sniffs_Arrays_
 				continue;
 			}
 
+			/*
+			if ( $tokens[ $index['index'] ]['column'] !== $indicesStart ) {
+				$expected = ( $indicesStart - 1 );
+				$found    = ( $tokens[ $index['index'] ]['column'] - 1 );
+				$error    = 'Array key not aligned correctly; expected %s spaces but found %s';
+				$data     = array(
+					$expected,
+					$found,
+				);
+
+				$fix = $phpcsFile->addFixableError( $error, $index['index'], 'KeyNotAligned', $data );
+				if ($fix === true) {
+					if ($found === 0) {
+						$phpcsFile->fixer->addContent( ( $index['index'] - 1 ), str_repeat(' ', $expected ) );
+					} else {
+						$phpcsFile->fixer->replaceToken( ( $index['index'] - 1 ), str_repeat(' ', $expected ) );
+					}
+				}
+
+				continue;
+			}
+
+			if ( $tokens[ $index['arrow'] ]['column'] !== $arrowStart ) {
+				$expected = ( $arrowStart - ( strlen( $index['index_content'] ) + $tokens[ $index['index'] ]['column'] ) );
+				$found    = ( $tokens[ $index['arrow'] ]['column'] - ( strlen( $index['index_content'] ) + $tokens[ $index['index'] ]['column'] ) );
+				$error    = 'Array double arrow not aligned correctly; expected %s space(s) but found %s';
+				$data     = array(
+					$expected,
+					$found,
+				);
+
+				$fix = $phpcsFile->addFixableError( $error, $index['arrow'], 'DoubleArrowNotAligned', $data );
+				if ($fix === true) {
+					if ($found === 0) {
+						$phpcsFile->fixer->addContent( ( $index['arrow'] - 1 ), str_repeat(' ', $expected ) );
+					} else {
+						$phpcsFile->fixer->replaceToken( ( $index['arrow'] - 1 ), str_repeat(' ', $expected ) );
+					}
+				}
+
+				continue;
+			}
+
+			if ( $tokens[ $index['value'] ]['column'] !== $valueStart ) {
+				$expected = ( $valueStart - ( $tokens[ $index['arrow'] ]['length'] + $tokens[ $index['arrow'] ]['column'] ) );
+				$found    = ( $tokens[ $index['value'] ]['column'] - ( $tokens[ $index['arrow'] ]['length'] + $tokens[ $index['arrow'] ]['column'] ) );
+				if ( $found < 0 ) {
+					$found = 'newline';
+				}
+
+				$error = 'Array value not aligned correctly; expected %s space(s) but found %s';
+				$data  = array(
+					$expected,
+					$found,
+				);
+
+				$fix = $phpcsFile->addFixableError( $error, $index['arrow'], 'ValueNotAligned', $data );
+				if ($fix === true) {
+					if ($found === 'newline') {
+						$prev = $phpcsFile->findPrevious( T_WHITESPACE, ( $index['value'] - 1 ), null, true );
+						$phpcsFile->fixer->beginChangeset();
+						for ( $i = ( $prev + 1 ); $i < $index['value']; $i++ ) {
+							$phpcsFile->fixer->replaceToken( $i, '' );
+						}
+
+						$phpcsFile->fixer->replaceToken( ( $index['value'] - 1 ), str_repeat(' ', $expected ) );
+						$phpcsFile->fixer->endChangeset();
+					} else if ($found === 0) {
+						$phpcsFile->fixer->addContent( ( $index['value'] - 1 ), str_repeat(' ', $expected ) );
+					} else {
+						$phpcsFile->fixer->replaceToken( ( $index['value'] - 1 ), str_repeat(' ', $expected ) );
+					}
+				}
+			} // end if
+			*/
+
 			// Check each line ends in a comma.
 			$valueLine = $tokens[ $index['value'] ]['line'];
 			$nextComma = false;
 			for ( $i = $index['value']; $i < $arrayEnd; $i++ ) {
 				// Skip bracketed statements, like function calls.
-				if ( T_OPEN_PARENTHESIS === $tokens[ $i ]['code'] ) {
+				if ($tokens[$i]['code'] === T_OPEN_PARENTHESIS) {
 					$i         = $tokens[ $i ]['parenthesis_closer'];
 					$valueLine = $tokens[ $i ]['line'];
 					continue;
 				}
 
-				if ( T_ARRAY === $tokens[ $i ]['code'] ) {
+				if ($tokens[$i]['code'] === T_ARRAY) {
 					$i         = $tokens[ $tokens[ $i ]['parenthesis_opener'] ]['parenthesis_closer'];
 					$valueLine = $tokens[ $i ]['line'];
 					continue;
 				}
 
-				if ( T_OPEN_SHORT_ARRAY === $tokens[ $i ]['code'] ) {
+				if ($tokens[$i]['code'] === T_OPEN_SHORT_ARRAY) {
 					$i         = $tokens[ $i ]['bracket_closer'];
 					$valueLine = $tokens[ $i ]['line'];
 					continue;
 				}
 
-				if ( T_CLOSURE === $tokens[ $i ]['code'] ) {
+				if ($tokens[$i]['code'] === T_CLOSURE) {
 					$i         = $tokens[ $i ]['scope_closer'];
 					$valueLine = $tokens[ $i ]['line'];
 					continue;
 				}
 
-				if ( T_COMMA === $tokens[ $i ]['code'] ) {
+				if ($tokens[$i]['code'] === T_COMMA) {
 					$nextComma = $i;
 					break;
 				}
-			} // End for().
+			}//end for
 
 			if ( false === $nextComma ) {
 				$error = 'Each line in an array declaration must end in a comma';
 				$fix   = $phpcsFile->addFixableError( $error, $index['value'], 'NoComma' );
 
-				if ( true === $fix ) {
+				if ($fix === true) {
 					// Find the end of the line and put a comma there.
 					for ( $i = ( $index['value'] + 1 ); $i < $phpcsFile->numTokens; $i++ ) {
 						if ( $tokens[ $i ]['line'] > $valueLine ) {
@@ -410,7 +580,7 @@ class WordPress_Sniffs_Arrays_ArrayDeclarationSniff extends Squiz_Sniffs_Arrays_
 			}
 
 			// Check that there is no space before the comma.
-			if ( false !== $nextComma && T_WHITESPACE === $tokens[ ( $nextComma - 1 ) ]['code'] ) {
+			if ($nextComma !== false && $tokens[($nextComma - 1)]['code'] === T_WHITESPACE) {
 				$content     = $tokens[ ( $nextComma - 2 ) ]['content'];
 				$spaceLength = $tokens[ ( $nextComma - 1 ) ]['length'];
 				$error       = 'Expected 0 spaces between "%s" and comma; %s found';
@@ -420,12 +590,12 @@ class WordPress_Sniffs_Arrays_ArrayDeclarationSniff extends Squiz_Sniffs_Arrays_
 				);
 
 				$fix = $phpcsFile->addFixableError( $error, $nextComma, 'SpaceBeforeComma', $data );
-				if ( true === $fix ) {
+				if ($fix === true) {
 					$phpcsFile->fixer->replaceToken( ( $nextComma - 1 ), '' );
 				}
 			}
-		} // End foreach().
+		}//end foreach
 
-	} // End processMultiLineArray().
+	}//end processMultiLineArray()
 
 } // End class.
