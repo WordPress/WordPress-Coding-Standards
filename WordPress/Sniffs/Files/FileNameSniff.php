2224b8c29c1c394b250abcc5ca1c509ecd064568
jrfnl <github_nospam@adviesenzo.nl>
2017-01-17 21:17:54 +0100

    FileNameSniff: Add checking if the filename is correctly based on the classname if the file contains a class.

--- WordPress/Sniffs/Files/FileNameSniff.php
+++ WordPress/Sniffs/Files/FileNameSniff.php
@@ -16,8 +16,36 @@
  *
  * @since   0.1.0
  * @since   0.11.0 - This sniff will now also check for all lowercase file names.
+ *                 - This sniff will now also verify that files containing a class start with `class-`.
  */
 class WordPress_Sniffs_Files_FileNameSniff implements PHP_CodeSniffer_Sniff {
+
+	/**
+	/**
+	 * Historical exceptions in WP core to the class name rule.
+	 *
+	 * @since 0.11.0
+	 *
+	 * @var array
+	 */
+	private $class_exceptions = array(
+		'class.wp-dependencies.php' => true,
+		'class.wp-scripts.php'      => true,
+		'class.wp-styles.php'       => true,
+	);
+
+	/**
+	 * Unit test version of the historical exceptions in WP core.
+	 *
+	 * @since 0.11.0
+	 *
+	 * @var array
+	 */
+	private $unittest_class_exceptions = array(
+		'class.wp-dependencies.inc' => true,
+		'class.wp-scripts.inc'      => true,
+		'class.wp-styles.inc'       => true,
+	);
 
 	/**
 	 * Returns an array of tokens this test wants to listen for.
@@ -25,8 +53,11 @@ class WordPress_Sniffs_Files_FileNameSniff implements PHP_CodeSniffer_Sniff {
 	 * @return array
 	 */
 	public function register() {
-		return array( T_OPEN_TAG );
+		if ( defined( 'PHP_CODESNIFFER_IN_TESTS' ) ) {
+			$this->class_exceptions = array_merge( $this->class_exceptions, $this->unittest_class_exceptions );
+		}
 
+		return array( T_OPEN_TAG );
 	}
 
 	/**
@@ -57,6 +88,27 @@ class WordPress_Sniffs_Files_FileNameSniff implements PHP_CodeSniffer_Sniff {
 		}
 		unset( $expected );
 
+		/*
+		 * Check files containing a class for the "class-" prefix.
+		 */
+		$has_class = $phpcsFile->findNext( T_CLASS, $stackPtr );
+		if ( false !== $has_class ) {
+			$class_name = $phpcsFile->getDeclarationName( $has_class );
+			$expected   = 'class-' . strtolower( str_replace( '_', '-', $class_name ) );
+
+			if ( substr( $fileName, 0, -4 ) !== $expected && ! isset( $this->class_exceptions[ $fileName ] ) ) {
+				$phpcsFile->addError(
+					'Class file names should be based on the class name with "class-" prepended. Expected %s, but found %s.',
+					0,
+					'InvalidClassFileName',
+					array(
+						$expected . '.php',
+						$fileName,
+					)
+				);
+			}
+			unset( $expected );
+		}
 		// Only run this sniff once per file, no need to run it again.
 		return ( $phpcsFile->numTokens + 1 );
 
