77496a262a95c1c0d5bfc45fc8ff192f601a4efc
Ulrich Pogson <grapplerulrich@gmail.com>
2017-01-22 08:34:26 +0100

    Test the rulesets

---
+++ bin/functions.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * WordPress Coding Standard.
+ *
+ * @package WPCS\WordPressCodingStandards
+ * @link    https://github.com/WordPress-Coding-Standards/WordPress-Coding-Standards
+ * @license https://opensource.org/licenses/MIT MIT
+ */
+
+/**
+ * This is a test function to help test the XML rulesets.
+ */
+function wpcs_test_function() {
+	return true;
+}
