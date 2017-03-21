a267336f9b3c7f23b9760fa2dcc11147d5979f7c
jrfnl <github_nospam@adviesenzo.nl>
2017-03-22 00:26:36 +0100

    AbstractArrayAssignmentRestrictions: Fix method documentation.
    The method documentation for the `getGroups()` method in the `AbstractArrayAssignmentRestrictionsSniff` was incorrect (belonged to the `AbstractVariableRestrictionsSniff`).
    
    Corrected now for both the abstract as well as the child classes.

--- WordPress/Sniffs/VIP/SlowDBQuerySniff.php
+++ WordPress/Sniffs/VIP/SlowDBQuerySniff.php
@@ -20,17 +20,6 @@ class WordPress_Sniffs_VIP_SlowDBQuerySniff extends WordPress_AbstractArrayAssig
 
 	/**
 	 * Groups of variables to restrict.
-	 * This should be overridden in extending classes.
-	 *
-	 * Example: groups => array(
-	 * 	'wpdb' => array(
-	 * 		'type'          => 'error' | 'warning',
-	 * 		'message'       => 'Dont use this one please!',
-	 * 		'variables'     => array( '$val', '$var' ),
-	 * 		'object_vars'   => array( '$foo->bar', .. ),
-	 * 		'array_members' => array( '$foo['bar']', .. ),
-	 * 	)
-	 * )
 	 *
 	 * @return array
 	 */
