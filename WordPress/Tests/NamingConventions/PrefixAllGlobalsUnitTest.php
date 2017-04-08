ddc4534fe6f5a73aeffe9815b9fee4ea101df938
jrfnl <github_nospam@adviesenzo.nl>
2017-04-08 09:30:00 +0200

    PrefixAllGlobals: Add additional unit tests for dynamic variables/hooks / issue #915

--- WordPress/Tests/NamingConventions/PrefixAllGlobalsUnitTest.php
+++ WordPress/Tests/NamingConventions/PrefixAllGlobalsUnitTest.php
@@ -26,30 +26,37 @@ class WordPress_Tests_NamingConventions_PrefixAllGlobalsUnitTest extends Abstrac
 		switch ( $testFile ) {
 			case 'PrefixAllGlobalsUnitTest.inc':
 				return array(
-					1   => 2, // 2 x error for incorrect prefix.
+					1   => 2, // 2 x error for incorrect prefix passed.
 					10  => 1,
 					18  => 1,
 					21  => 1,
 					22  => 1,
+					23  => 1,
+					24  => 1,
 					25  => 1,
-					27  => 1,
 					28  => 1,
 					30  => 1,
 					31  => 1,
-					32  => 1,
+					33  => 1,
 					34  => 1,
 					35  => 1,
-					79  => 1,
-					80  => 1,
-					134 => ( PHP_VERSION_ID >= 50300 ) ? 0 : 1, // PHPCS on PHP 5.2 does not recognize namespaces.
-					136 => ( PHP_VERSION_ID >= 50300 ) ? 0 : 1, // PHPCS on PHP 5.2 does not recognize namespaces.
-					138 => ( PHP_VERSION_ID >= 50300 ) ? 0 : 1, // PHPCS on PHP 5.2 does not recognize namespaces.
-					139 => ( PHP_VERSION_ID >= 50300 ) ? 0 : 1, // PHPCS on PHP 5.2 does not recognize namespaces.
-					140 => ( PHP_VERSION_ID >= 50300 ) ? 0 : 1, // PHPCS on PHP 5.2 does not recognize namespaces.
-					209 => ( function_exists( 'mb_strpos' ) ) ? 0 : 1,
-					214 => ( function_exists( 'array_column' ) ) ? 0 : 1,
-					218 => ( defined( 'E_DEPRECATED' ) ) ? 0 : 1,
-					222 => ( class_exists( 'IntlTimeZone' ) ) ? 0 : 1,
+					37  => 1,
+					38  => 1,
+					39  => 1,
+					40  => 1,
+					90  => 1,
+					91  => 1,
+					// Scoped.
+					149 => ( PHP_VERSION_ID >= 50300 ) ? 0 : 1, // PHPCS on PHP 5.2 does not recognize namespaces.
+					151 => ( PHP_VERSION_ID >= 50300 ) ? 0 : 1, // PHPCS on PHP 5.2 does not recognize namespaces.
+					153 => ( PHP_VERSION_ID >= 50300 ) ? 0 : 1, // PHPCS on PHP 5.2 does not recognize namespaces.
+					154 => ( PHP_VERSION_ID >= 50300 ) ? 0 : 1, // PHPCS on PHP 5.2 does not recognize namespaces.
+					155 => ( PHP_VERSION_ID >= 50300 ) ? 0 : 1, // PHPCS on PHP 5.2 does not recognize namespaces.
+					// Backfills.
+					225 => ( function_exists( 'mb_strpos' ) ) ? 0 : 1,
+					230 => ( function_exists( 'array_column' ) ) ? 0 : 1,
+					234 => ( defined( 'E_DEPRECATED' ) ) ? 0 : 1,
+					238 => ( class_exists( 'IntlTimeZone' ) ) ? 0 : 1,
 				);
 
 			case 'PrefixAllGlobalsUnitTest.1.inc':
@@ -77,10 +84,54 @@ class WordPress_Tests_NamingConventions_PrefixAllGlobalsUnitTest extends Abstrac
 	/**
 	 * Returns the lines where warnings should occur.
 	 *
+	 * @param string $testFile The name of the file being tested.
 	 * @return array <int line number> => <int number of warnings>
 	 */
-	public function getWarningList() {
-		return array();
+	public function getWarningList( $testFile = 'PrefixAllGlobalsUnitTest.inc' ) {
+
+		switch ( $testFile ) {
+			case 'PrefixAllGlobalsUnitTest.inc':
+				return array(
+					249 => 1,
+					250 => 1,
+					253 => 1,
+					254 => 1,
+					255 => 1,
+					256 => 1,
+					257 => 1,
+					258 => 1,
+					259 => 1,
+					260 => 1,
+					261 => 1,
+					263 => 1,
+					264 => 1,
+					265 => 1,
+					266 => 1,
+					267 => 1,
+					269 => 1,
+					270 => 1,
+					271 => 1,
+					272 => 1,
+					273 => 1,
+					274 => 1,
+					275 => 1,
+					286 => 1,
+					287 => 1,
+					288 => 1,
+					290 => 1,
+					291 => 1,
+					292 => 1,
+					293 => 1,
+					295 => 1,
+					296 => 1,
+					297 => 1,
+					299 => 1,
+				);
+
+			default:
+				return array();
+
+		} // End switch().
 
 	}
 
