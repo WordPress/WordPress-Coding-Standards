3682c0df4c4343c4759256acc3c742f4d4c05b76
jrfnl <github_nospam@adviesenzo.nl>
2017-05-06 21:57:54 +0200

    :sparkles: New sniff to verify that spaces are used for mid-line alignment
    > Tabs should be used at the beginning of the line for indentation, while spaces can be used mid-line for alignment.
    
    This new sniff covers the second part of this rule. (The first part is already covered by the `Generic.WhiteSpace.DisallowSpaceIndent` sniff).
    
    The error which is thrown is auto-fixable.
    
    The new sniff has been added to the `WordPress-Core` ruleset.

---
+++ WordPress/Tests/WhiteSpace/DisallowInlineTabsUnitTest.php
@@ -0,0 +1,43 @@
+<?php
+/**
+ * Unit test class for WordPress Coding Standard.
+ *
+ * @package WPCS\WordPressCodingStandards
+ * @link    https://github.com/WordPress-Coding-Standards/WordPress-Coding-Standards
+ * @license https://opensource.org/licenses/MIT MIT
+ */
+
+/**
+ * Unit test class for the DisallowInlineTabs sniff.
+ *
+ * @package WPCS\WordPressCodingStandards
+ * @since   0.12.0
+ */
+class WordPress_Tests_WhiteSpace_DisallowInlineTabsUnitTest extends AbstractSniffUnitTest {
+
+	/**
+	 * Returns the lines where errors should occur.
+	 *
+	 * @return array <int line number> => <int number of errors>
+	 */
+	public function getErrorList() {
+		return array(
+			17 => 1,
+			22 => 1,
+			23 => 1,
+			24 => 1,
+			26 => 1,
+		);
+	}
+
+	/**
+	 * Returns the lines where warnings should occur.
+	 *
+	 * @return array <int line number> => <int number of warnings>
+	 */
+	public function getWarningList() {
+		return array();
+
+	}
+
+} // End class.
