cc5f92d10d93648939154efe2f0c416b436f44d1
jrfnl <jrfnl@users.noreply.github.com>
2017-06-24 19:58:58 +0200

    ArrayDeclarationSpacing sniff: remove array indentation from the associative array fixer
    ... as that is handled by the `ArrayIndentation` sniff which was introduced in PR 941

--- WordPress/Sniffs/Arrays/ArrayDeclarationSpacingSniff.php
+++ WordPress/Sniffs/Arrays/ArrayDeclarationSpacingSniff.php
@@ -131,128 +131,37 @@ class WordPress_Sniffs_Arrays_ArrayDeclarationSpacingSniff extends WordPress_Sni
 			);
 
 			if ( true === $fix ) {
-				// Only deal with one nesting level per loop to have the best chance of getting the indentation right.
-				static $current_loop = array();
 
-				if ( ! isset( $current_loop[ $this->phpcsFile->fixer->loops ] ) ) {
-					$current_loop[ $this->phpcsFile->fixer->loops ] = array_fill( 0, $this->phpcsFile->numTokens, false );
+				$array_items = $this->get_function_call_parameters( $stackPtr );
+				if ( empty( $array_items ) ) {
+					// Strange, no array items found.
+					return;
 				}
 
-				if ( false === $current_loop[ $this->phpcsFile->fixer->loops ][ $opener ] ) {
-					for ( $i = $opener; $i <= $closer; $i++ ) {
-						$current_loop[ $this->phpcsFile->fixer->loops ][ $i ] = true;
+				$this->phpcsFile->fixer->beginChangeset();
+
+				foreach( $array_items as $item ) {
+					/*
+					 * Add a line break before the first non-empty token in the array item.
+					 * Prevents extraneous whitespace at the start of the line which could be
+					 * interpreted as alignment whitespace.
+					 */
+					$first_non_empty = $this->phpcsFile->findNext(
+						PHP_CodeSniffer_Tokens::$emptyTokens,
+						$item['start'],
+						( $item['end'] + 1 ),
+						true
+					);
+					if ( false === $first_non_empty ) {
+						continue;
 					}
 
-					$this->fix_associative_array( $this->phpcsFile, $opener, $closer );
+					$this->phpcsFile->fixer->addNewlineBefore( $first_non_empty );
 				}
+
+				$this->phpcsFile->fixer->endChangeset();
 			}
 		}
 	}
-
-	/**
-	 * Create & apply a changeset for a single line array with associative keys.
-	 *
-	 * @since 0.11.0
-	 *
-	 * @param PHP_CodeSniffer_File $phpcsFile  The file being scanned.
-	 * @param int                  $arrayStart Position of the array opener in the token stack.
-	 * @param int                  $arrayEnd   Position of the array closer in the token stack.
-	 * @return void
-	 */
-	protected function fix_associative_array( PHP_CodeSniffer_File $phpcsFile, $arrayStart, $arrayEnd ) {
-
-		$tokens = $phpcsFile->getTokens();
-
-		// Determine the needed indentation.
-		$indentation = '';
-		for ( $i = $arrayStart; $i >= 0; $i-- ) {
-			if ( $tokens[ $i ]['line'] === $tokens[ $arrayStart ]['line'] ) {
-				continue;
-			}
-
-			if ( T_WHITESPACE === $tokens[ ( $i + 1 ) ]['code'] ) {
-				// If the tokenizer replaced tabs with spaces, use the original content.
-				$indentation = $tokens[ ( $i + 1 ) ]['content'];
-				if ( isset( $tokens[ ( $i + 1 ) ]['orig_content'] ) ) {
-					$indentation = $tokens[ ( $i + 1 ) ]['orig_content'];
-				}
-			}
-			break;
-		}
-		unset( $i );
-
-		$value_indentation = "\t" . $indentation;
-
-		// Which nesting level is the one we are interested in ?
-		$nesting_count = 1;
-		if ( T_OPEN_SHORT_ARRAY === $tokens[ $arrayStart ]['code'] ) {
-			$nesting_count = 0;
-		}
-
-		if ( isset( $tokens[ $arrayStart ]['nested_parenthesis'] ) ) {
-			$nesting_count += count( $tokens[ $arrayStart ]['nested_parenthesis'] );
-		}
-
-		// Record the required changes.
-		$phpcsFile->fixer->beginChangeset();
-
-		$phpcsFile->fixer->addNewline( $arrayStart );
-		if ( T_WHITESPACE === $tokens[ ( $arrayStart + 1 ) ]['code'] ) {
-			$phpcsFile->fixer->replaceToken( ( $arrayStart + 1 ), $value_indentation );
-		} else {
-			$phpcsFile->fixer->addContentBefore( ( $arrayStart + 1 ), $value_indentation );
-		}
-
-		for ( $ptr = ( $arrayStart + 1 ); $ptr < $arrayEnd; $ptr++ ) {
-			$ptr = $phpcsFile->findNext( array( T_COMMA, T_OPEN_SHORT_ARRAY ), $ptr, $arrayEnd );
-
-			if ( false === $ptr ) {
-				break;
-			}
-
-			// Ignore anything within short array definition brackets.
-			// Necessary as the nesting level in that case is still the same.
-			if ( 'T_OPEN_SHORT_ARRAY' === $tokens[ $ptr ]['type']
-				&& ( isset( $tokens[ $ptr ]['bracket_opener'] )
-					&& $tokens[ $ptr ]['bracket_opener'] === $ptr )
-				&& isset( $tokens[ $ptr ]['bracket_closer'] )
-			) {
-				$ptr = $tokens[ $ptr ]['bracket_closer'];
-				continue;
-			}
-
-			// Ignore comma's at a lower nesting level.
-			if ( 'T_COMMA' === $tokens[ $ptr ]['type']
-				&& isset( $tokens[ $ptr ]['nested_parenthesis'] )
-				&& count( $tokens[ $ptr ]['nested_parenthesis'] ) !== $nesting_count
-			) {
-				continue;
-			}
-
-			$phpcsFile->fixer->addNewline( $ptr );
-			if ( isset( $tokens[ ( $ptr + 1 ) ] ) ) {
-				if ( T_WHITESPACE === $tokens[ ( $ptr + 1 ) ]['code'] ) {
-					$phpcsFile->fixer->replaceToken( ( $ptr + 1 ), $value_indentation );
-				} else {
-					$phpcsFile->fixer->addContentBefore( ( $ptr + 1 ), $value_indentation );
-				}
-			}
-		}
-
-		$token_before_end = $phpcsFile->findPrevious( PHP_CodeSniffer_Tokens::$emptyTokens, ( $arrayEnd - 1 ), $arrayStart, true, null, true );
-		if ( 'T_COMMA' !== $tokens[ $token_before_end ]['type'] ) {
-			$phpcsFile->fixer->addContent( $token_before_end, ',' );
-
-			if ( T_WHITESPACE === $tokens[ ( $arrayEnd - 1 ) ]['code'] || "\n" === $phpcsFile->fixer->getTokenContent( ( $arrayEnd - 1 ) ) ) {
-				$phpcsFile->fixer->replaceToken( ( $arrayEnd - 1 ), "\n" . $indentation );
-			} else {
-				$phpcsFile->fixer->addContentBefore( $arrayEnd, "\n" . $indentation );
-			}
-		} elseif ( $value_indentation === $phpcsFile->fixer->getTokenContent( ( $arrayEnd - 1 ) ) ) {
-			$phpcsFile->fixer->replaceToken( ( $arrayEnd - 1 ), $indentation );
-		}
-
-		$phpcsFile->fixer->endChangeset();
-	} // End fix_associative_array().
 
 } // End class.
