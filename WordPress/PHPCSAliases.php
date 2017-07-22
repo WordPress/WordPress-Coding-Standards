3112d5a16598afe7ee25bd667a088a464a5ea651
jrfnl <jrfnl@users.noreply.github.com>
2017-07-22 20:37:32 +0200

    PHPCS 3.x compat: Alias PHPCS 3.x classes to their PHPCS 2.x counterparts
    Notes:
    * The file is automatically loaded by PHPCS using the new `<autoload>` option. This option, when encountered in the ruleset, is ignored by previous PHPCS versions, so can be used safely and will automatically make sure the file is only loaded for PHPCS 3.x.
    * The file is loaded after the PHPCS native autoloader has started, but before any sniffs are loaded making for a clean way to add the aliases and prevent fatal 'class not found' errors.
    * Each class alias statement has to be wrapped in a `class_exists` condition to prevent fatal `class already declared` errors when this ruleset is used in combination with other external ruleset(s) which use the same/similar methodology to create PHPCS cross-version compatibility.
    
    The `<autoload>` ruleset directive was added in PHPCS 3.0.1 in response to issue squizlabs/PHP_CodeSniffer#1469

---
+++ WordPress/PHPCSAliases.php
@@ -0,0 +1,50 @@
+<?php
+/**
+ * PHPCS cross-version compatibility helper.
+ *
+ * @package WPCS\WordPressCodingStandards
+ * @link    https://github.com/WordPress-Coding-Standards/WordPress-Coding-Standards
+ * @license https://opensource.org/licenses/MIT MIT
+ * @since   0.13.0
+ */
+
+/*
+ * Alias a number of PHPCS 3.x classes to their PHPCS 2.x equivalents.
+ *
+ * This file is auto-loaded by PHPCS 3.x before any sniffs are loaded
+ * through the PHPCS 3.x `<autoload>` ruleset directive.
+ *
+ * {@internal The PHPCS files have been reorganized in PHPCS 3.x, quite
+ * a few "old" classes have been split and spread out over several "new"
+ * classes. In other words, this will only work for a limited number
+ * of classes.}}
+ *
+ * {@internal The `class_exists` wrappers are needed to play nice with other
+ * external PHPCS standards creating cross-version compatibility in the same
+ * manner.}}
+ */
+if ( ! defined( 'WPCS_PHPCS_ALIASES_SET' ) ) {
+	// PHPCS base classes/interface.
+	if ( ! interface_exists( '\PHP_CodeSniffer_File' ) ) {
+		class_alias( 'PHP_CodeSniffer\Files\File', '\PHP_CodeSniffer_File' );
+	}
+	if ( ! class_exists( '\PHP_CodeSniffer_Tokens' ) ) {
+		class_alias( 'PHP_CodeSniffer\Util\Tokens', '\PHP_CodeSniffer_Tokens' );
+	}
+	if ( ! class_exists( '\PHP_CodeSniffer_Sniff' ) ) {
+		class_alias( 'PHP_CodeSniffer\Sniffs\Sniff', '\PHP_CodeSniffer_Sniff' );
+	}
+
+	// PHPCS classes which are being extended by WPCS sniffs.
+	if ( ! class_exists( '\PHP_CodeSniffer_Standards_AbstractVariableSniff' ) ) {
+		class_alias( 'PHP_CodeSniffer\Sniffs\AbstractVariableSniff', '\PHP_CodeSniffer_Standards_AbstractVariableSniff' );
+	}
+	if ( ! class_exists( '\PEAR_Sniffs_NamingConventions_ValidFunctionNameSniff' ) ) {
+		class_alias( 'PHP_CodeSniffer\Standards\PEAR\Sniffs\NamingConventions\ValidFunctionNameSniff', '\PEAR_Sniffs_NamingConventions_ValidFunctionNameSniff' );
+	}
+	if ( ! class_exists( '\Squiz_Sniffs_WhiteSpace_OperatorSpacingSniff' ) ) {
+		class_alias( 'PHP_CodeSniffer\Standards\Squiz\Sniffs\WhiteSpace\OperatorSpacingSniff', '\Squiz_Sniffs_WhiteSpace_OperatorSpacingSniff' );
+	}
+
+	define( 'WPCS_PHPCS_ALIASES_SET', true );
+}
