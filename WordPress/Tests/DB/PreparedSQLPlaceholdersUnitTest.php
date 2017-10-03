867927059d7fbe901e05353bcf348d2b73703234
jrfnl <jrfnl@users.noreply.github.com>
2017-10-03 21:18:10 +0200

    PreparedSQLPlaceholders: minor improvements to the test case file
    * Test with multi-line heredoc.
    * Improve readability of the inline comments.

--- WordPress/Tests/DB/PreparedSQLPlaceholdersUnitTest.php
+++ WordPress/Tests/DB/PreparedSQLPlaceholdersUnitTest.php
@@ -27,19 +27,20 @@ class PreparedSQLPlaceholdersUnitTest extends AbstractSniffUnitTest {
 	 */
 	public function getErrorList() {
 		return array(
-			12 => 3,
-			13 => 2,
-			14 => 2,
-			15 => 2,
-			18 => 1,
-			19 => 1,
-			20 => 1,
-			22 => 2,
-			28 => 1,
-			29 => 1,
-			30 => 1,
-			31 => 1,
-			32 => 1,
+			17  => 3,
+			19  => 2,
+			20  => 2,
+			21  => 2,
+			26  => 1,
+			27  => 1,
+			28  => 1,
+			32  => 1,
+			33  => 1,
+			41  => 1,
+			42  => 1,
+			43  => 1,
+			44  => 1,
+			45  => 1,
 		);
 	}
 
@@ -50,7 +51,7 @@ class PreparedSQLPlaceholdersUnitTest extends AbstractSniffUnitTest {
 	 */
 	public function getWarningList() {
 		return array(
-			9 => 1,
+			12  => 1,
 		);
 
 	}
