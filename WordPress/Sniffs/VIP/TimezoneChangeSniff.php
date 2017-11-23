52ee64485153ae40bc6ed6cb16576de55eb80899
jrfnl <jrfnl@users.noreply.github.com>
2017-11-23 16:50:10 +0100

    TimezoneChange: move sniff from VIP category to WP category
    The original sniff class still exists, but extends the renamed sniff now.
    In addition, a deprecation warning will be thrown if this sniff is included directly by a custom ruleset.
    A deprecation warning will also be thrown if the sniff is included directly just to set custom properties.
    
    Note: if the sniff was explicitly **excluded** via a custom ruleset, no deprecation warning will be thrown and the exclusion will have no effect. The custom ruleset will need to be updated to use the new sniff name for the exclusion to be reinstated.

--- WordPress/Sniffs/VIP/TimezoneChangeSniff.php
+++ WordPress/Sniffs/VIP/TimezoneChangeSniff.php
@@ -14,7 +14,7 @@ use WordPress\AbstractFunctionRestrictionsSniff;
 /**
  * Disallow the changing of timezone.
  *
- * @link    http://vip.wordpress.com/documentation/use-current_time-not-date_default_timezone_set/
+ * @link    https://vip.wordpress.com/documentation/use-current_time-not-date_default_timezone_set/
  *
  * @package WPCS\WordPressCodingStandards
  *
@@ -22,32 +22,58 @@ use WordPress\AbstractFunctionRestrictionsSniff;
  * @since   0.11.0 Extends the WordPress_AbstractFunctionRestrictionsSniff instead of the
  *                 Generic_Sniffs_PHP_ForbiddenFunctionsSniff.
  * @since   0.13.0 Class name changed: this class is now namespaced.
+ *
+ * @deprecated 0.15.0 This sniff has been moved to the `WP` category.
+ *                    This file remains for now to prevent BC breaks.
  */
-class TimezoneChangeSniff extends AbstractFunctionRestrictionsSniff {
+class TimezoneChangeSniff extends \WordPress\Sniffs\WP\TimezoneChangeSniff {
 
 	/**
-	 * Groups of functions to restrict.
+	 * Keep track of whether the warnings have been thrown to prevent
+	 * the messages being thrown for every token triggering the sniff.
 	 *
-	 * Example: groups => array(
-	 *  'lambda' => array(
-	 *      'type'      => 'error' | 'warning',
-	 *      'message'   => 'Use anonymous functions instead please!',
-	 *      'functions' => array( 'file_get_contents', 'create_function' ),
-	 *  )
-	 * )
+	 * @since 0.15.0
 	 *
-	 * @return array
+	 * @var array
 	 */
-	public function getGroups() {
-		return array(
-			'timezone_change' => array(
-				'type'      => 'error',
-				'message'   => 'Using %s() and similar isn\'t allowed, instead use WP internal timezone support.',
-				'functions' => array(
-					'date_default_timezone_set',
-				),
-			),
-		);
-	} // End getGroups().
+	private $thrown = array(
+		'DeprecatedSniff'                 => false,
+		'FoundPropertyForDeprecatedSniff' => false,
+	);
+
+	/**
+	 * Don't use.
+	 *
+	 * @deprecated 0.15.0
+	 *
+	 * @param int $stackPtr The position of the current token in the stack.
+	 *
+	 * @return void|int
+	 */
+	public function process_token( $stackPtr ) {
+		if ( false === $this->thrown['DeprecatedSniff'] ) {
+			$this->phpcsFile->addWarning(
+				'The "WordPress.VIP.TimezoneChange" sniff has been renamed to "WordPress.VIP.TimezoneChange". Please update your custom ruleset.',
+				0,
+				'DeprecatedSniff'
+			);
+
+			$this->thrown['DeprecatedSniff'] = true;
+		}
+
+		if ( ! empty( $this->exclude )
+			&& false === $this->thrown['FoundPropertyForDeprecatedSniff']
+		) {
+			$this->phpcsFile->addWarning(
+				'The "WordPress.VIP.TimezoneChange" sniff has been renamed to "WordPress.VIP.TimezoneChange". Please update your custom ruleset.',
+				0,
+				'FoundPropertyForDeprecatedSniff'
+			);
+
+			$this->thrown['FoundPropertyForDeprecatedSniff'] = true;
+		}
+
+		return parent::process_token( $stackPtr );
+	}
 
 } // End class.
--- WordPress/Tests/WP/TimezoneChangeUnitTest.php
+++ WordPress/Tests/WP/TimezoneChangeUnitTest.php
@@ -7,7 +7,7 @@
  * @license https://opensource.org/licenses/MIT MIT
  */
 
-namespace WordPress\Tests\VIP;
+namespace WordPress\Tests\WP;
 
 use PHP_CodeSniffer\Tests\Standards\AbstractSniffUnitTest;
 
@@ -18,9 +18,7 @@ use PHP_CodeSniffer\Tests\Standards\AbstractSniffUnitTest;
  *
  * @since   0.3.0
  * @since   0.13.0 Class name changed: this class is now namespaced.
- * @since   0.15.0 The sniff has been deprecated. This unit test file now
- *                 only tests that the deprecation warnings are correctly thrown
- *                 and that the sniff falls through to the parent correctly.
+ * @since   0.15.0 This sniff has been moved from the `VIP` category to the `WP` category.
  */
 class TimezoneChangeUnitTest extends AbstractSniffUnitTest {
 
@@ -31,7 +29,7 @@ class TimezoneChangeUnitTest extends AbstractSniffUnitTest {
 	 */
 	public function getErrorList() {
 		return array(
-			4 => 1,
+			3 => 1,
 		);
 
 	}
@@ -42,9 +40,7 @@ class TimezoneChangeUnitTest extends AbstractSniffUnitTest {
 	 * @return array <int line number> => <int number of warnings>
 	 */
 	public function getWarningList() {
-		return array(
-			1 => 2,
-		);
+		return array();
 
 	}
 
