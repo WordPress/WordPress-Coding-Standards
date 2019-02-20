bb93c38c29d9f16bed536050577cad8bf9392cf2
Niels de Blaauw <niels@level-level.com>
2019-02-20 13:21:27 +0100

    Fixes unit-test for removed ini_set check

--- WordPress/Tests/PHP/DiscouragedPHPFunctionsUnitTest.php
+++ WordPress/Tests/PHP/DiscouragedPHPFunctionsUnitTest.php
@@ -50,18 +50,17 @@ class DiscouragedPHPFunctionsUnitTest extends AbstractSniffUnitTest {
 			22 => 1,
 			23 => 1,
 			24 => 1,
-			25 => 1,
+			27 => 1,
 			28 => 1,
 			29 => 1,
 			30 => 1,
 			31 => 1,
-			32 => 1,
+			34 => 1,
 			35 => 1,
 			36 => 1,
 			37 => 1,
 			38 => 1,
 			39 => 1,
-			40 => 1,
 		);
 	}
 
