fa0e10aae39b5226894ef1e22c7be549b048bc4a
jrfnl <jrfnl@users.noreply.github.com>
2020-04-10 01:41:42 +0200

    PHPCSHelper: deprecate the class and all the methods in it
    As everything here is now contained within PHPCSUtils, including updated and/or improved versions of some of these.

--- WordPress/PHPCSHelper.php
+++ WordPress/PHPCSHelper.php
@@ -11,6 +11,7 @@ namespace WordPressCS\WordPress;
 
 use PHP_CodeSniffer\Config;
 use PHP_CodeSniffer\Files\File;
+use PHPCSUtils\BackCompat\Helper;
 
 /**
  * PHPCSHelper
@@ -22,7 +23,8 @@ use PHP_CodeSniffer\Files\File;
  *
  * @package WPCS\WordPressCodingStandards
  *
- * @since   0.13.0
+ * @since      0.13.0
+ * @deprecated 3.0.0  Use {@see PHPCSUtils\BackCompat\Helper} instead.
  */
 class PHPCSHelper {
 
@@ -34,7 +36,7 @@ class PHPCSHelper {
 	 * @return string
 	 */
 	public static function get_version() {
-		return Config::VERSION;
+		return Helper::getVersion();
 	}
 
 	/**
@@ -42,7 +44,8 @@ class PHPCSHelper {
 	 *
 	 * PHPCS cross-version compatibility helper.
 	 *
-	 * @since 0.13.0
+	 * @since      0.13.0
+	 * @deprecated 3.0.0
 	 *
 	 * @param string      $key   The name of the config value.
 	 * @param string|null $value The value to set. If null, the config entry
@@ -51,59 +54,49 @@ class PHPCSHelper {
 	 *                           This will not write the config data to the config file.
 	 */
 	public static function set_config_data( $key, $value, $temp = false ) {
-		Config::setConfigData( $key, $value, $temp );
+		Helper::setConfigData( $key, $value, $temp );
 	}
 
 	/**
 	 * Get the value of a single PHPCS config key.
 	 *
-	 * @since 0.13.0
+	 * @since      0.13.0
+	 * @deprecated 3.0.0
 	 *
 	 * @param string $key The name of the config value.
 	 *
 	 * @return string|null
 	 */
 	public static function get_config_data( $key ) {
-		return Config::getConfigData( $key );
+		return Helper::getConfigData( $key );
 	}
 
 	/**
 	 * Get the tab width as passed to PHPCS from the command-line or the ruleset.
 	 *
-	 * @since 0.13.0
+	 * @since      0.13.0
+	 * @deprecated 3.0.0
 	 *
 	 * @param \PHP_CodeSniffer\Files\File $phpcsFile The file being processed.
 	 *
 	 * @return int Tab width. Defaults to 4.
 	 */
 	public static function get_tab_width( File $phpcsFile ) {
-		$tab_width = 4;
-
-		if ( isset( $phpcsFile->config->tabWidth ) && $phpcsFile->config->tabWidth > 0 ) {
-			$tab_width = $phpcsFile->config->tabWidth;
-		}
-
-		return $tab_width;
+		return Helper::getTabWidth( $phpcsFile );
 	}
 
 	/**
 	 * Check whether the `--ignore-annotations` option has been used.
 	 *
-	 * @since 0.13.0
+	 * @since      0.13.0
+	 * @deprecated 3.0.0
 	 *
 	 * @param \PHP_CodeSniffer\Files\File $phpcsFile Optional. The current file being processed.
 	 *
 	 * @return bool True if annotations should be ignored, false otherwise.
 	 */
 	public static function ignore_annotations( File $phpcsFile = null ) {
-		if ( isset( $phpcsFile, $phpcsFile->config->annotations ) ) {
-			return ! $phpcsFile->config->annotations;
-		} else {
-			$annotations = Config::getConfigData( 'annotations' );
-			if ( isset( $annotations ) ) {
-				return ! $annotations;
-			}
-		}
+		return Helper::ignoreAnnotations( $phpcsFile );
 	}
 
 }
