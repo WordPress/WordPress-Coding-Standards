610e001cc4eb124c2acba1989aef984a2549b167
Anton Vlasenko <vlasenko.anton@gmail.com>
2022-09-16 22:17:33 +0200

    Commit WIP.

--- WordPress/Sniffs/NamingConventions/ValidGutenbergFunctionName.php
+++ WordPress/Sniffs/NamingConventions/ValidGutenbergFunctionName.php
@@ -9,11 +9,6 @@
 
 namespace WordPressCS\WordPress\Sniffs\NamingConventions;
 
-use PHPCSUtils\BackCompat\BCTokens;
-use PHPCSUtils\Utils\FunctionDeclarations;
-use PHPCSUtils\Utils\ObjectDeclarations;
-use PHPCSUtils\Utils\Scopes;
-use WordPressCS\WordPress\Helpers\DeprecationHelper;
 use WordPressCS\WordPress\Sniff;
 
 /**
@@ -31,14 +26,14 @@ use WordPressCS\WordPress\Sniff;
  * @since   3.0.0  This sniff has been refactored and no longer extends the upstream
  *                 PEAR.NamingConventions.ValidFunctionName sniff.
  */
-class ValidGutenbergFunctionName extends Sniff {
+class ValidGutenbergFunctionNameSniff extends Sniff {
 
 	/**
 	 * Returns an array of tokens this test wants to listen for.
 	 *
+	 * @return array
 	 * @since 3.0.0
 	 *
-	 * @return array
 	 */
 	public function register() {
 		return array( \T_FUNCTION );
@@ -47,64 +42,27 @@ class ValidGutenbergFunctionName extends Sniff {
 	/**
 	 * Processes this test, when one of its tokens is encountered.
 	 *
-	 * @since 3.0.0
-	 *
 	 * @param int $stackPtr The position of the current token in the stack.
 	 *
 	 * @return int|void Integer stack pointer to skip forward or void to continue
 	 *                  normal file processing.
+	 * @since 3.0.0
+	 *
 	 */
 	public function process_token( $stackPtr ) {
+		$functionName = $this->phpcsFile->getDeclarationName( $stackPtr );
 
-		$name = FunctionDeclarations::getName( $this->phpcsFile, $stackPtr );
-		if ( empty( $name ) === true ) {
-			// Live coding or parse error.
-			return;
+		if ( preg_match( '/^_?gutenberg_[^\s]+$/', $functionName ) ) {
+			// The function has a valid prefix.
+			return null;
 		}
 
-		$ooPtr = Scopes::validDirectScope( $this->phpcsFile, $stackPtr, BCTokens::ooScopeTokens() );
-		// Not interested in methods, functions only.
-		if ( true === $ooPtr ) {
-			return;
-		}
 
-		$this->process_function_declaration( $stackPtr, $name );
+		$error      = 'The function should have a gutenberg prefix.';
+		$error_code = 'IncorrectFunctionPrefix';
+
+		// Don't auto-fix: Something other than whitespace found between keyword and open parenthesis.
+		$this->phpcsFile->addError( $error, $stackPtr, $error_code );
 	}
 
-	/**
-	 * Processes a function declaration for a function in the global namespace.
-	 *
-	 * @since 0.1.0
-	 * @since 3.0.0 Renamed from `processTokenOutsideScope()` to `process_function_declaration()`.
-	 *              Method signature has been changed as well as this method no longer overloads
-	 *              a method from the PEAR sniff which was previously the sniff parent.
-	 *
-	 * @param int    $stackPtr     The position where this token was found.
-	 * @param string $functionName The name of the function.
-	 *
-	 * @return void
-	 */
-	protected function process_function_declaration( $stackPtr, $functionName ) {
-
-		// PHP magic functions are exempt from our rules.
-		if ( FunctionDeclarations::isMagicFunctionName( $functionName ) === true ) {
-			return;
-		}
-
-		// Is the function name prefixed with "__" ?
-		if ( preg_match( '`^__[^_]`', $functionName ) === 1 ) {
-			$error     = 'Function name "%s" is invalid; only PHP magic methods should be prefixed with a double underscore';
-			$errorData = array( $functionName );
-			$this->phpcsFile->addError( $error, $stackPtr, 'FunctionDoubleUnderscore', $errorData );
-		}
-
-		if ( strtolower( $functionName ) !== $functionName ) {
-			$error     = 'Function name "%s" is not in snake case format, try "%s"';
-			$errorData = array(
-				$functionName,
-				$this->get_snake_case_name_suggestion( $functionName ),
-			);
-			$this->phpcsFile->addError( $error, $stackPtr, 'FunctionNameInvalid', $errorData );
-		}
-	}
 }
