653a981c44a46666a6f08b76217ec0bcb04f5e3c
jrfnl <jrfnl@users.noreply.github.com>
2022-12-11 11:33:21 +0100

    WP/EnqueuedResources: add tests with PHP 7.3+ flexible heredoc/nowdocs
    .. to safeguard these are handled correctly.
    
    Note: these are in a separate file for maintainability as any code which would be placed _after_ the flexible heredoc/nowdoc would not be tokenized correctly on PHP < 7.3, making the tests maintainer error prone if the tests were in the same file.

--- WordPress/Tests/WP/EnqueuedResourcesUnitTest.php
+++ WordPress/Tests/WP/EnqueuedResourcesUnitTest.php
@@ -24,33 +24,56 @@ class EnqueuedResourcesUnitTest extends AbstractSniffUnitTest {
 	/**
 	 * Returns the lines where errors should occur.
 	 *
+	 * @param string $testFile The name of the file being tested.
+	 *
 	 * @return array <int line number> => <int number of errors>
 	 */
-	public function getErrorList() {
-		return array(
-			1  => 1,
-			2  => 1,
-			6  => 1,
-			7  => 1,
-			10 => 1,
-			11 => 1,
-			13 => 1,
-			14 => 1,
-			16 => 1,
-			17 => 1,
-			20 => 1,
-			21 => 1,
-			25 => 1,
-			26 => 1,
-			30 => 1,
-			31 => 1,
-			42 => 1,
-			46 => 1,
-			48 => 1,
-			49 => 1,
-			54 => 1,
-			55 => 1,
-		);
+	public function getErrorList( $testFile = '' ) {
+		switch ( $testFile ) {
+			case 'EnqueuedResourcesUnitTest.1.inc':
+				return array(
+					1  => 1,
+					2  => 1,
+					6  => 1,
+					7  => 1,
+					10 => 1,
+					11 => 1,
+					13 => 1,
+					14 => 1,
+					16 => 1,
+					17 => 1,
+					20 => 1,
+					21 => 1,
+					25 => 1,
+					26 => 1,
+					30 => 1,
+					31 => 1,
+					42 => 1,
+					46 => 1,
+					48 => 1,
+					49 => 1,
+					54 => 1,
+					55 => 1,
+				);
+
+			case 'EnqueuedResourcesUnitTest.2.inc':
+				// These tests will only yield reliable results when PHPCS is run on PHP 7.3 or higher.
+				if ( \PHP_VERSION_ID < 70300 ) {
+					return array();
+				}
+
+				return array(
+					7  => 1,
+					8  => 1,
+					12 => 1,
+					13 => 1,
+					17 => 1,
+					18 => 1,
+				);
+
+			default:
+				return array();
+		}
 	}
 
 	/**
@@ -61,5 +84,4 @@ class EnqueuedResourcesUnitTest extends AbstractSniffUnitTest {
 	public function getWarningList() {
 		return array();
 	}
-
 }
