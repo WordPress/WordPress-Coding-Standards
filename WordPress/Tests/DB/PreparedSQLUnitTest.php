a756ab6626535ff3cc5fce0b770c56e0ed9368cb
jrfnl <jrfnl@users.noreply.github.com>
2023-01-08 07:16:12 +0100

    DB/PreparedSQL: add test with PHP 7.3+ flexible heredoc
    ... to safeguard the sniff handles this correctly, in the same way as "normal" heredocs.
    
    Note: the tests are in a separate file for maintainability as any code which would be placed _after_ the flexible heredoc/nowdoc would not be tokenized correctly on PHP < 7.3, making the tests maintainer error prone if the tests were in the same file.

--- WordPress/Tests/DB/PreparedSQLUnitTest.php
+++ WordPress/Tests/DB/PreparedSQLUnitTest.php
@@ -25,43 +25,59 @@ class PreparedSQLUnitTest extends AbstractSniffUnitTest {
 	/**
 	 * Returns the lines where errors should occur.
 	 *
-	 * @since 0.8.0
+	 * @param string $testFile The name of the file being tested.
 	 *
 	 * @return array <int line number> => <int number of errors>
 	 */
-	public function getErrorList() {
-		return array(
-			3   => 1,
-			4   => 1,
-			5   => 1,
-			7   => 1,
-			8   => 1,
-			11  => 1, // Old-style WPCS ignore comments are no longer supported.
-			12  => 1, // Old-style WPCS ignore comments are no longer supported.
-			16  => 1,
-			17  => 1,
-			18  => 1,
-			20  => 1,
-			21  => 1,
-			54  => 1,
-			64  => 1,
-			71  => 1,
-			85  => 1,
-			90  => 1,
-			106 => 1,
-			107 => 1,
-			108 => 1,
-			109 => 1,
-			112 => 1,
-			115 => 1,
-			118 => 1,
-			120 => 1,
-			121 => 1,
-			123 => 1,
-			124 => 1,
-			128 => 1,
-			132 => 2,
-		);
+	public function getErrorList( $testFile = '' ) {
+		switch ( $testFile ) {
+			case 'PreparedSQLUnitTest.1.inc':
+				return array(
+					3   => 1,
+					4   => 1,
+					5   => 1,
+					7   => 1,
+					8   => 1,
+					11  => 1, // Old-style WPCS ignore comments are no longer supported.
+					12  => 1, // Old-style WPCS ignore comments are no longer supported.
+					16  => 1,
+					17  => 1,
+					18  => 1,
+					20  => 1,
+					21  => 1,
+					54  => 1,
+					64  => 1,
+					71  => 1,
+					85  => 1,
+					90  => 1,
+					106 => 1,
+					107 => 1,
+					108 => 1,
+					109 => 1,
+					112 => 1,
+					115 => 1,
+					118 => 1,
+					120 => 1,
+					121 => 1,
+					123 => 1,
+					124 => 1,
+					128 => 1,
+					132 => 2,
+				);
+
+			case 'PreparedSQLUnitTest.2.inc':
+				// These tests will only yield reliable results when PHPCS is run on PHP 7.3 or higher.
+				if ( \PHP_VERSION_ID < 70300 ) {
+					return array();
+				}
+
+				return array(
+					7 => 1,
+				);
+
+			default:
+				return array();
+		}
 	}
 
 	/**
