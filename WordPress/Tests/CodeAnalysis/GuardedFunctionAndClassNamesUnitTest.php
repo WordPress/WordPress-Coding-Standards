d5b1b580e4c84ecfc8e66e148e50fdb372eb82c2
Anton Vlasenko <vlasenko.anton@gmail.com>
2023-03-24 14:23:06 +0100

    Add unit tests.

---
+++ WordPress/Tests/CodeAnalysis/GuardedFunctionAndClassNamesUnitTest.php
@@ -0,0 +1,44 @@
+<?php
+/**
+ * Unit test class for WordPress Coding Standard.
+ *
+ * @package WPCS\WordPressCodingStandards
+ * @link    https://github.com/WordPress/WordPress-Coding-Standards
+ * @license https://opensource.org/licenses/MIT MIT
+ */
+
+
+use PHP_CodeSniffer\Tests\Standards\AbstractSniffUnitTest;
+
+/**
+ * Unit test class for the GuardedFunctionAndClassNames sniff.
+ *
+ * @package WPCS\WordPressCodingStandards
+ *
+ * @since   2.2.0
+ */
+final class GuardedFunctionAndClassNamesUnitTest extends AbstractSniffUnitTest {
+
+	/**
+	 * Returns the lines where errors should occur.
+	 *
+	 * @return array <int line number> => <int number of errors>
+	 */
+	public function getErrorList() {
+		return array();
+	}
+
+	/**
+	 * Returns the lines where warnings should occur.
+	 *
+	 * @return array <int line number> => <int number of warnings>
+	 */
+	public function getWarningList() {
+		return array(
+			6 => 1,
+			7 => 1,
+			8 => 1,
+		);
+	}
+
+}
