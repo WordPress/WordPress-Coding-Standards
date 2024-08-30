63906877e87ff2034d3cac84e637c39ec4466c16
davidperezgar <david@closemarketing.es>
2024-08-30 17:40:02 +0200

    updated based on feedbak

--- WordPress/Sniffs/NamingConventions/PrefixAllGlobalsSniff.php
+++ WordPress/Sniffs/NamingConventions/PrefixAllGlobalsSniff.php
@@ -58,8 +58,9 @@ final class PrefixAllGlobalsSniff extends AbstractFunctionParameterSniff {
 	/**
 	 * Minimal number of characters the prefix needs in order to be valid.
 	 *
-	 * @since n.e.x.t.
+	 * @since 2.2.0
 	 *
+	 * @link https://github.com/WordPress/WordPress-Coding-Standards/issues/1733 Issue 1733.
 	 * @link https://github.com/WordPress/WordPress-Coding-Standards/issues/2467 Issue 2467.
 	 *
 	 * @var int
