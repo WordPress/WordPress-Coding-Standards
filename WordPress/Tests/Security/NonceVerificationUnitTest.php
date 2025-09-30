359d901417b6a83ed8b22100f96e0c20d07ca195
Rodrigo Primo <rodrigosprimo@gmail.com>
2025-09-30 15:29:42 +0530

    ContextHelper::is_in_type_test(): add tests

--- WordPress/Tests/Security/NonceVerificationUnitTest.php
+++ WordPress/Tests/Security/NonceVerificationUnitTest.php
@@ -18,7 +18,6 @@ use PHP_CodeSniffer\Tests\Standards\AbstractSniffUnitTest;
  * @since 0.13.0 Class name changed: this class is now namespaced.
  * @since 1.0.0  This sniff has been moved from the `CSRF` category to the `Security` category.
  *
- * @covers \WordPressCS\WordPress\Helpers\ContextHelper::is_in_type_test
  * @covers \WordPressCS\WordPress\Sniffs\Security\NonceVerificationSniff
  */
 final class NonceVerificationUnitTest extends AbstractSniffUnitTest {
