54670c6618f2319dd2537bcb19291f7dd4df476b
Rodrigo Primo <rodrigosprimo@gmail.com>
2025-12-04 16:13:03 -0300

    PR 2716 PrintingFunctionsTrait::is_printing_function(): add tests

---
+++ WordPress/Tests/Helpers/PrintingFunctionsTrait/IsPrintingFunctionUnitTest.php
@@ -0,0 +1,84 @@
+<?php
+/**
+ * WordPress Coding Standard.
+ *
+ * @package WPCS\WordPressCodingStandards
+ * @link    https://github.com/WordPress/WordPress-Coding-Standards
+ * @license https://opensource.org/licenses/MIT MIT
+ */
+
+namespace WordPressCS\WordPress\Tests\Helpers\PrintingFunctionsTrait;
+
+use PHPUnit\Framework\TestCase;
+use WordPressCS\WordPress\Helpers\PrintingFunctionsTrait;
+
+/**
+ * Tests for the `PrintingFunctionsTrait::is_printing_function()` method.
+ *
+ * @since 3.4.0
+ *
+ * @covers \WordPressCS\WordPress\Helpers\PrintingFunctionsTrait::is_printing_function
+ */
+final class IsPrintingFunctionUnitTest extends TestCase {
+
+	/**
+	 * Test class using the PrintingFunctionsTrait for testing purposes.
+	 *
+	 * @var object
+	 */
+	private static $testClass;
+
+	/**
+	 * Set up the test class.
+	 *
+	 * @beforeClass
+	 *
+	 * @return void
+	 */
+	public static function setUpBeforeClass(): void {
+		self::$testClass = new class() {
+			use PrintingFunctionsTrait;
+		};
+	}
+
+	/**
+	 * Test is_printing_function().
+	 *
+	 * @dataProvider dataIsPrintingFunction
+	 *
+	 * @param string $functionName   The function name to test.
+	 * @param bool   $expectedResult The expected return value.
+	 *
+	 * @return void
+	 */
+	public function testIsPrintingFunction( $functionName, $expectedResult ) {
+		$this->assertSame(
+			$expectedResult,
+			self::$testClass->is_printing_function( $functionName )
+		);
+	}
+
+	/**
+	 * Data provider.
+	 *
+	 * @see testIsPrintingFunction()
+	 *
+	 * @return array<string, array<string, bool|string>>
+	 */
+	public static function dataIsPrintingFunction() {
+		return array(
+			'lowercase_name'          => array(
+				'functionName'   => 'printf',
+				'expectedResult' => true,
+			),
+			'mixedcase_name'          => array(
+				'functionName'   => 'vPrInTf',
+				'expectedResult' => true,
+			),
+			'not_a_printing_function' => array(
+				'functionName'   => 'echo',
+				'expectedResult' => false,
+			),
+		);
+	}
+}
