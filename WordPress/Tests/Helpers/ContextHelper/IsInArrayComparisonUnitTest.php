94dfbaa6dba642548695cc977476948eb9008053
Rodrigo Primo <rodrigosprimo@gmail.com>
2026-04-24 10:11:41 -0300

    Add a bare variable test for consistency with the other similar helper method tests

--- WordPress/Tests/Helpers/ContextHelper/IsInArrayComparisonUnitTest.php
+++ WordPress/Tests/Helpers/ContextHelper/IsInArrayComparisonUnitTest.php
@@ -48,6 +48,10 @@ final class IsInArrayComparisonUnitTest extends UtilityMethodTestCase {
 	public static function dataIsInArrayComparison() {
 		return array(
 			// Cases that should return false.
+			'bare_variable' => array(
+				'testMarker'     => '/* testBareVariable */',
+				'expectedResult' => false,
+			),
 			'other_function_call' => array(
 				'testMarker'     => '/* testOtherFunctionCall */',
 				'expectedResult' => false,
