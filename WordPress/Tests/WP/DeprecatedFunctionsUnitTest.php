a4e1404d9b835c257da6444b7e7f3bb484cdc4ea
Rodrigo Primo <rodrigosprimo@gmail.com>
2026-07-01 18:46:56 +0000

    WP/DeprecatedFunctions: update the functions list based on WP 7.0.0
    Based on a scan of WP Core at commit WordPress/wordpress-develop@26b6802 (the WP 7.0.0 release tag) using a preliminary sniff created for issue #1803.
    
    Includes tests.

--- WordPress/Tests/WP/DeprecatedFunctionsUnitTest.php
+++ WordPress/Tests/WP/DeprecatedFunctionsUnitTest.php
@@ -113,14 +113,15 @@ final class DeprecatedFunctionsUnitTest extends AbstractSniffUnitTest {
 		switch ( $testFile ) {
 			case 'DeprecatedFunctionsUnitTest.1.inc':
 				$start_line = 430;
-				$end_line   = 446;
+				$end_line   = 450;
 				$warnings   = array_fill( $start_line, ( ( $end_line - $start_line ) + 1 ), 1 );
 
 				// Unset the lines related to version comments.
 				unset(
 					$warnings[432],
 					$warnings[442],
-					$warnings[444]
+					$warnings[444],
+					$warnings[447]
 				);
 
 				return $warnings;
